3bf37a8625f8440b363114969d87bf7d44fe7bb3
DwGoing <jianghy1209@163.com>
2020-01-15 00:41:30 +0800

    测试项目待调整

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,6 +1,4 @@
-﻿#define Database // 测试项目
-
-using System;
+﻿using System;
 using System.IO;
 using System.Text;
 using System.Linq;
@@ -18,43 +16,41 @@ namespace Test
     {
         static void Main(string[] args)
         {
-#if Core
             ServiceHost host = new ServiceHost();
             host.RegisterConfiguration($"{Directory.GetCurrentDirectory()}/CoreTest", "Config.json");
             host.RegisterFromAssembly("Test"); // 从程序集注入
             var provider = host.Build();
             var service = provider.GetService<ITestInterface, TestClass1>();
             service.TestMethod("helo");
-#elif Http
-            ServiceHost host = new ServiceHost();
-            host.RegisterConfiguration($"{Directory.GetCurrentDirectory()}/HttpTest", "Config.json");
-            host.RegisterHttpService();
-            var provider = host.Build();
-            provider.InitHttpService<HttpStartup>();
-#elif WebSocket
-            ServiceHost host = new ServiceHost();
-            host.RegisterConfiguration($"{Directory.GetCurrentDirectory()}/WebsocketTest", "Config.json");
-            host.RegisterWebSocketService();
-            var provider = host.Build();
-            provider.InitWebSocketService(async (msg, webSocket) =>
-            {
-                // 对消息自定义处理
-                Console.WriteLine(msg);
-                byte[] msgBytes = Encoding.UTF8.GetBytes(msg);
-                await webSocket.SendAsync(new ArraySegment<byte>(msgBytes), System.Net.WebSockets.WebSocketMessageType.Text, true, CancellationToken.None);
-                if (msg == "close")
-                    await webSocket.CloseOutputAsync(System.Net.WebSockets.WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
-            });
-#elif Database
-            ServiceHost host = new ServiceHost();
-            host.RegisterConfiguration($"{Directory.GetCurrentDirectory()}/DatabaseTest", "Config.json");
-            host.RegisterDatabaseService();
-            var provider = host.Build();
-            provider.InitDatabaseService();
-            var service = provider.GetService<IDatabaseService, DatabaseService>();
-            var res = service.QueryWithCache<TestTable>(item => item.Id == 3);
-            Console.WriteLine(res.ToJson());
-#endif
+
+            //ServiceHost host = new ServiceHost();
+            //host.RegisterConfiguration($"{Directory.GetCurrentDirectory()}/HttpTest", "Config.json");
+            //host.RegisterHttpService();
+            //var provider = host.Build();
+            //provider.InitHttpService<HttpStartup>();
+
+            //ServiceHost host = new ServiceHost();
+            //host.RegisterConfiguration($"{Directory.GetCurrentDirectory()}/WebsocketTest", "Config.json");
+            //host.RegisterWebSocketService();
+            //var provider = host.Build();
+            //provider.InitWebSocketService(async (msg, webSocket) =>
+            //{
+            //    // 对消息自定义处理
+            //    Console.WriteLine(msg);
+            //    byte[] msgBytes = Encoding.UTF8.GetBytes(msg);
+            //    await webSocket.SendAsync(new ArraySegment<byte>(msgBytes), System.Net.WebSockets.WebSocketMessageType.Text, true, CancellationToken.None);
+            //    if (msg == "close")
+            //        await webSocket.CloseOutputAsync(System.Net.WebSockets.WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
+            //});
+
+            //ServiceHost host = new ServiceHost();
+            //host.RegisterConfiguration($"{Directory.GetCurrentDirectory()}/DatabaseTest", "Config.json");
+            //host.RegisterDatabaseService();
+            //var provider = host.Build();
+            //provider.InitDatabaseService();
+            //var service = provider.GetService<IDatabaseService, DatabaseService>();
+            //var res = service.QueryWithCache<TestTable>(item => item.Id == 3);
+            //Console.WriteLine(res.ToJson());
         }
     }
 }
