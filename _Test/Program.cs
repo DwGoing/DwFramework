ea6e63c0d47ded05da6b37de0912bd41409962e2
DwGoing <jianghy1209@163.com>
2020-01-22 01:00:27 +0800

    新增RpcService测试

--- _Test/Program.cs
+++ _Test/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using DwFramework.Core;
+using DwFramework.Core.Extensions;
+using DwFramework.Rpc;
+
+using Hprose.RPC;
 
 namespace _Test
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ServiceHost host = new ServiceHost();
+            host.RegisterConfiguration(Directory.GetCurrentDirectory(), "Config.json");
+            host.RegisterRpcService();
+            var provider = host.Build();
+            await provider.InitRpcServiceAsync();
+            var rpc = provider.GetService<IRpcService, RpcService>();
+            rpc.Service.Add<string>(T);
+
+            var client = new Client("http://127.0.0.1:10010/");
+            client.Invoke("T", new object[] { "helo" });
+
+            Console.ReadLine();
+        }
+
+        public static void T(string str)
+        {
+            Console.WriteLine(str);
         }
     }
 }
