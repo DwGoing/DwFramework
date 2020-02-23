e10f9dd60f4deafe714387e77b2ede50f79f80f1
DwGoing <jianghy1209@163.com>
2020-02-23 19:29:10 +0800

    分离扩展

---
+++ WebSocket/Extensions/WebSocketExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using DwFramework.Core;
+using DwFramework.Core.Extensions;
+
+namespace DwFramework.WebSocket.Extensions
+{
+    public static class WebSocketExtension
+    {
+        /// <summary>
+        /// 注册WebSocket服务
+        /// </summary>
+        /// <param name="host"></param>
+        public static void RegisterWebSocketService(this ServiceHost host)
+        {
+            host.RegisterType<IWebSocketService, WebSocketService>().SingleInstance();
+        }
+
+        /// <summary>
+        /// 初始化WebSocket服务
+        /// </summary>
+        /// <param name="provider"></param>
+        /// <param name="handler"></param>
+        public static Task InitWebSocketServiceAsync(this IServiceProvider provider, OnConnectHandler onConnect = null, OnSendHandler onSend = null, OnReceiveHandler onReceive = null, OnCloseHandler onClose = null)
+        {
+            var service = provider.GetService<IWebSocketService, WebSocketService>();
+            if (onConnect != null) service.OnConnect += onConnect;
+            if (onSend != null) service.OnSend += onSend;
+            if (onReceive != null) service.OnReceive += onReceive;
+            if (onClose != null) service.OnClose += onClose;
+            return service.OpenServiceAsync();
+        }
+    }
+}
--- WebSocket/WebSocketService.cs
+++ WebSocket/WebSocketService.cs
@@ -15,33 +15,6 @@ using DwFramework.Core.Extensions;
 
 namespace DwFramework.WebSocket
 {
-    public static class WebSocketServiceExtension
-    {
-        /// <summary>
-        /// 注册WebSocket服务
-        /// </summary>
-        /// <param name="host"></param>
-        public static void RegisterWebSocketService(this ServiceHost host)
-        {
-            host.RegisterType<IWebSocketService, WebSocketService>().SingleInstance();
-        }
-
-        /// <summary>
-        /// 初始化WebSocket服务
-        /// </summary>
-        /// <param name="provider"></param>
-        /// <param name="handler"></param>
-        public static Task InitWebSocketServiceAsync(this IServiceProvider provider, OnConnectHandler onConnect = null, OnSendHandler onSend = null, OnReceiveHandler onReceive = null, OnCloseHandler onClose = null)
-        {
-            var service = provider.GetService<IWebSocketService, WebSocketService>();
-            if (onConnect != null) service.OnConnect += onConnect;
-            if (onSend != null) service.OnSend += onSend;
-            if (onReceive != null) service.OnReceive += onReceive;
-            if (onClose != null) service.OnClose += onClose;
-            return service.OpenServiceAsync();
-        }
-    }
-
     public class WebSocketService : IWebSocketService
     {
         public class Config
