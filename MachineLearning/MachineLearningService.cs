33ebe7622709e64c603dd450d1b0a89997ca4f96
DwGoing <jianghy1209@163.com>
2020-03-08 01:32:42 +0800

    实现简单的分类及回归预测

--- MachineLearning/MachineLearningService.cs
+++ MachineLearning/MachineLearningService.cs
@@ -1,48 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 
 using Microsoft.ML;
-using Microsoft.ML.Data;
-using static Microsoft.ML.DataOperationsCatalog;
-using Microsoft.ML.Trainers;
-using Microsoft.ML.Transforms.Text;
-
-using DwFramework.Core;
-using DwFramework.Core.Extensions;
 
 namespace DwFramework.MachineLearning
 {
     public class MachineLearningService : IMachineLearningService
     {
-        public readonly MLContext MLContext;
+        public readonly MLContext MlContext;
+        public DataOperationsCatalog Data { get { return MlContext.Data; } }
+        public TransformsCatalog Transforms { get { return MlContext.Transforms; } }
+        public ModelOperationsCatalog Model { get { return MlContext.Model; } }
+
+        public BinaryClassificationCatalog BinaryClassification { get { return MlContext.BinaryClassification; } }
+        public BinaryClassificationCatalog.BinaryClassificationTrainers BinaryClassificationTrainers { get { return MlContext.BinaryClassification.Trainers; } }
+        public RegressionCatalog Regression { get { return MlContext.Regression; } }
+        public RegressionCatalog.RegressionTrainers RegressionTrainers { get { return MlContext.Regression.Trainers; } }
 
         /// <summary>
         /// 构造函数
         /// </summary>
         public MachineLearningService()
         {
-            MLContext = new MLContext();
+            MlContext = new MLContext();
         }
 
-        public ITransformer BuildAndEvaluateModel(IDataView data, string inputColumnName, double trainRate = 0.1)
+        /// <summary>
+        /// 数据加载处理
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <param name="handle"></param>
+        /// <returns></returns>
+        public IDataView DataLoad<T>(IEnumerable<T> data, Func<IDataView, DataOperationsCatalog, IDataView> handle = null) where T : class
         {
-            var trainTestData = MLContext.Data.TrainTestSplit(data, trainRate);
-            var estimator = MLContext.Transforms.Text.FeaturizeText("Features", inputColumnName)
-                .Append(MLContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
-            Console.WriteLine("=============== Building Model ===============");
-            var model = estimator.Fit(trainTestData.TrainSet);
-            Console.WriteLine("=============== Evaluating Model ===============");
-            IDataView predictions = model.Transform(trainTestData.TestSet);
-            CalibratedBinaryClassificationMetrics metrics = MLContext.BinaryClassification.Evaluate(predictions, "Label");
-            Console.WriteLine("Model quality metrics evaluation");
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
-            Console.WriteLine($"Auc: {metrics.AreaUnderRocCurve:P2}");
-            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
-            Console.WriteLine("--------------------------------");
-            return model;
+            var sourceData = Data.LoadFromEnumerable(data);
+            return handle == null ? sourceData : handle(sourceData, Data);
         }
+
+        /// <summary>
+        /// 数据加载处理
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="path"></param>
+        /// <param name="handle"></param>
+        /// <param name="separatorChar"></param>
+        /// <param name="hasHeader"></param>
+        /// <param name="allowQuoting"></param>
+        /// <param name="trimWhitespace"></param>
+        /// <param name="allowSparse"></param>
+        /// <returns></returns>
+        public IDataView DataLoad<T>(string path, Func<IDataView, DataOperationsCatalog, IDataView> handle = null, char separatorChar = '\t', bool hasHeader = false, bool allowQuoting = false, bool trimWhitespace = false, bool allowSparse = false) where T : class
+        {
+            var sourceData = Data.LoadFromTextFile<T>(path, separatorChar, hasHeader, allowQuoting, trimWhitespace, allowSparse);
+            return handle == null ? sourceData : handle(sourceData, Data);
+        }
+
+        /// <summary>
+        /// 数据加载处理
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="paths"></param>
+        /// <param name="handle"></param>
+        /// <param name="separatorChar"></param>
+        /// <param name="hasHeader"></param>
+        /// <param name="allowQuoting"></param>
+        /// <param name="trimWhitespace"></param>
+        /// <param name="allowSparse"></param>
+        /// <returns></returns>
+        public IDataView DataLoad<T>(string[] paths, Func<IDataView, DataOperationsCatalog, IDataView> handle = null, char separatorChar = '\t', bool hasHeader = false, bool allowQuoting = false, bool trimWhitespace = false, bool allowSparse = false) where T : class
+        {
+            var loader = Data.CreateTextLoader<T>(separatorChar, hasHeader, null, allowQuoting, trimWhitespace, allowSparse);
+            var sourceData = loader.Load(paths);
+            return handle == null ? sourceData : handle(sourceData, Data);
+        }
+
+        /// <summary>
+        /// 数据转换处理
+        /// </summary>
+        /// <param name="handle"></param>
+        /// <returns></returns>
+        public IEstimator<ITransformer> BuildDataTranferPipeline(Func<TransformsCatalog, IEstimator<ITransformer>> handle) => handle(Transforms);
     }
 }
