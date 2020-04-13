106a8210ff97e7927dbf8a07f6e8a0276b60098b
DwGoing <jianghy1209@163.com>
2020-04-13 09:29:52 +0800

    新增请求过滤器插件

---
+++ Web/Models/RequestFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+
+namespace DwFramework.Web
+{
+    public static class RequestFilter
+    {
+        public static IApplicationBuilder UseRequestFilter(this IApplicationBuilder app, Action<HttpContext> startHandler, Action<HttpContext> endHandler)
+        {
+            app.Use(async (context, next) =>
+            {
+                try
+                {
+                    startHandler(context);
+                    await next();
+                    endHandler(context);
+                }
+                catch (Exception ex)
+                {
+                    await context.Response.WriteAsync(ex.Message);
+                }
+            });
+            return app;
+        }
+    }
+}
