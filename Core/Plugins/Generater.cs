cdb40cbef3fb13de9df356d8b3c60f7ee583b6d6
DwGoing <jianghy1209@163.com>
2020-04-19 14:24:36 +0800

    优化流水号生成

--- Core/Plugins/Generater.cs
+++ Core/Plugins/Generater.cs
@@ -24,10 +24,14 @@ namespace DwFramework.Core.Plugins
 
         /// <summary>
         /// 生成流水号
-        /// [日期 6位][时间 4位][随机数 6位][自定义号段1 4位][自定义号段2 4位]
+        /// [日期 6位][时间 4位][随机数 6位][自定义号段1 4位][自定义号段2 4位][盐 4位]
         /// </summary>
-        /// <param name="workId"></param>
+        /// <param name="customNum1"></param>
+        /// <param name="customNum2"></param>
         /// <returns></returns>
+        private static object _uuidLock = new object();
+        private static int _uuidNorce = 0;
+        private static string _lastTimeTag = DateTime.Now.ToString("HHmmss");
         public static string GenerateUUID(string customNum1 = "0000", string customNum2 = "0000")
         {
             StringBuilder builder = new StringBuilder();
@@ -47,6 +51,19 @@ namespace DwFramework.Core.Plugins
                 throw new Exception("自定义号段长度错误");
             builder.Append(customNum1);
             builder.Append(customNum2);
+            lock (_uuidLock)
+            {
+                if (_lastTimeTag == nowTime.ToString("HHmmss"))
+                {
+                    _uuidNorce++;
+                }
+                else
+                {
+                    _lastTimeTag = nowTime.ToString("HHmmss");
+                    _uuidNorce = 0;
+                }
+            }
+            builder.Append(_uuidNorce);
             return builder.ToString();
         }
     }
