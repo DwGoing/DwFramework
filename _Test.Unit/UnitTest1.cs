f29309872d4484d4900133b86fe76e89566565bf
DwGoing <jianghy1209@163.com>
2020-05-06 10:28:39 +0800

    新增单元测试

---
+++ _Test.Unit/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _Test.Unit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
