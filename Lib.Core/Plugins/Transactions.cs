62264987b9591cc452c598b207cc2dbcbcba6d15
DwGoing <jianghy1209@163.com>
2020-06-26 18:27:08 +0800

    添加事务操作

---
+++ Lib.Core/Plugins/Transactions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DwFramework.Core.Plugins
+{
+    public class Transaction
+    {
+        public string ID;
+
+        private List<(Action Action, Action<Exception> RollbackAction)> _actions = new List<(Action Action, Action<Exception> RollbackAction)>();
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public Transaction()
+        {
+            ID = Generater.GenerateGUID().ToString();
+        }
+
+        /// <summary>
+        /// 添加操作
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="rollbackAction"></param>
+        public void AddAction(Action action, Action<Exception> rollbackAction)
+        {
+            _actions.Add((action, rollbackAction));
+        }
+
+        /// <summary>
+        /// 清除所有操作
+        /// </summary>
+        public void ClearAction()
+        {
+            _actions.Clear();
+        }
+
+        /// <summary>
+        /// 执行事务
+        /// </summary>
+        public void Invoke()
+        {
+            foreach (var item in _actions)
+            {
+                try
+                {
+                    item.Action.Invoke();
+                }
+                catch (Exception ex)
+                {
+                    item.RollbackAction.Invoke(ex);
+                    break;
+                }
+            }
+        }
+    }
+}
--- _Test.Core/Program.cs
+++ _Test.Core/Program.cs
@@ -16,7 +16,13 @@ namespace _Test.Core
         {
             try
             {
-                Console.WriteLine(DateTime.Parse("2020-06-27").GetTimeDiff());
+                var t = new Transaction();
+                t.AddAction(() => { Console.WriteLine(1); }, ex => { Console.WriteLine(1 + ex.Message); });
+                t.AddAction(() => { Console.WriteLine(2); }, ex => { Console.WriteLine(2 + ex.Message); });
+                t.AddAction(() => { Console.WriteLine(3); throw new Exception("Test Exception!"); }, ex => { Console.WriteLine(3 + ex.Message); });
+                t.AddAction(() => { Console.WriteLine(4); }, ex => { Console.WriteLine(4 + ex.Message); });
+                t.AddAction(() => { Console.WriteLine(5); }, ex => { Console.WriteLine(5 + ex.Message); });
+                t.Invoke();
                 //var value = "osnfweiniocnownowenionweoinoiwneoidweonedowneoiweendwiondowne lwei we cweindenfwelinfiwlenfsfjoiwejdweifnfosnfweiniocnownowenionweoinoiwneoidweonedowneoiweendwiondowne lwei we cweindenfwelinfiwlenfsfjoiwejdweifnfosnfweiniocnownowenionweoinoiwneoidweonedowneoiweendwiondowne lwei we cweindenfwelinfiwlenfsfjoiwejdweifnfosnfweiniocnownowenionweoinoiwneoidweonedowneoiweendwiondowne lwei we cweindenfwelinfiwlenfsfjoiwejdweifnfosnfweiniocnownowenionweoinoiwneoidweonedowneoiweendwiondowne lwei we cweindenfwelinfiwlenfsfjoiwejdweifnfosnfweiniocnownowenionweoinoiwneoidweonedowneoiweendwiondowne lwei we cweindenfwelinfiwlenfsfjoiwejdweifnfosnfweiniocnownowenionweoinoiwneoidweonedowneoiweendwiondowne lwei we cweindenfwelinfiwlenfsfj1282689";
                 //var keys = EncryptUtil.Rsa.GenerateKeyPair(RSAExtensions.RSAKeyType.Pkcs1, 1024);
                 //var s1 = EncryptUtil.Rsa.EncryptWithPrivateKey(value, RSAExtensions.RSAKeyType.Pkcs1, keys.PrivateKey);
