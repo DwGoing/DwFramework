f106c4fa1503f0de5691ba757b8f38130d03a313
DwGoing <jianghy1209@163.com>
2020-06-29 23:11:32 +0800

    新增测试代码

--- _Test.Rpc/Program.cs
+++ _Test.Rpc/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 using DwFramework.Core;
 using DwFramework.Core.Extensions;
 using DwFramework.Rpc;
@@ -17,17 +18,22 @@ namespace _Test.Rpc
             {
                 var host = new ServiceHost(configFilePath: $"{AppDomain.CurrentDomain.BaseDirectory}Config.json");
                 host.RegisterInstance(new A());
+                host.RegisterType<C>();
                 host.RegisterRpcService();
                 host.InitService(provider =>
                 {
                     var rpc = provider.GetRpcService();
                     rpc.Service.AddMethod("AA", provider.GetService<A>());
+                    rpc.Service.Add(B.BB);
+                    rpc.RegisterFuncFromInstance(provider.GetService<C>());
                 });
                 Task.Run(() =>
                 {
                     Thread.Sleep(5000);
                     var client = new Client("http://127.0.0.1:10100");
                     client.Invoke("AA");
+                    client.Invoke("BB");
+                    client.Invoke("CC");
                 });
                 host.Run();
             }
@@ -45,6 +51,23 @@ namespace _Test.Rpc
         public void AA()
         {
             Console.WriteLine("aa");
+        }
+    }
+
+    public class B
+    {
+        public static void BB()
+        {
+            Console.WriteLine("bb");
+        }
+    }
+
+    public class C
+    {
+        [Rpc("CC")]
+        public void CC()
+        {
+            Console.WriteLine("cc");
         }
     }
 }
