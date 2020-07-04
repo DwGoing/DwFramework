5e67e0f62cccf7b824339e6e2d8d5c920c611394
DwGoing <jianghy1209@163.com>
2020-07-04 10:55:14 +0800

    优化uuid生成规则

--- Lib.Core/Plugins/Generater.cs
+++ Lib.Core/Plugins/Generater.cs
@@ -33,39 +33,38 @@ namespace DwFramework.Core.Plugins
 
         /// <summary>
         /// 生成流水号
-        /// [日期 6位][时间 4位][随机数 6位][自定义号段 4位][盐 8位]
+        /// [日期 8位][时间 4位][自定义标识 n位][随机数 4位][Nonce 4位]
         /// </summary>
         /// <param name="customNum"></param>
         /// <returns></returns>
         private static object _uuidLock = new object();
-        private static int _uuidNorce = 0;
+        private static int _uuidNonce = 0;
         private static string _lastTimeTag = DateTime.Now.ToString("HHmmss");
-        public static string GenerateUUID(string customNum = "0000")
+        public static string GenerateUUID(string customTag = null)
         {
             StringBuilder builder = new StringBuilder();
             DateTime nowTime = DateTime.Now;
             // 日期+时间
             builder.Append(nowTime.ToString("yyyyMMddHHmm"));
             Random random = new Random(nowTime.Second * 1000 + nowTime.Millisecond);
+            // 自定义标识
+            if (customTag != null) builder.Append(customTag);
             // 随机数
-            builder.Append(random.Next(1000000).ToString().PadLeft(6, '0'));
-            // 自定义号段
-            if (customNum.Length != 4)
-                throw new Exception("自定义号段长度错误");
-            builder.Append(customNum);
+            builder.Append(random.Next(10000).ToString().PadLeft(4, '0'));
+            // Nonce
             lock (_uuidLock)
             {
                 if (_lastTimeTag == nowTime.ToString("HHmmss"))
                 {
-                    _uuidNorce++;
+                    _uuidNonce++;
                 }
                 else
                 {
                     _lastTimeTag = nowTime.ToString("HHmmss");
-                    _uuidNorce = 0;
+                    _uuidNonce = 0;
                 }
             }
-            builder.Append(_uuidNorce.ToString().PadLeft(8, '0'));
+            builder.Append(_uuidNonce.ToString().PadLeft(4, '0'));
             return builder.ToString();
         }
     }
