fe16fc753cb77c2a2e350b3054c6bb3850402a35
DwGoing <jianghy1209@163.com>
2020-07-21 15:27:08 +0800

    移除测试代码

--- _Test.Core/Program.cs
+++ _Test.Core/Program.cs
@@ -27,18 +27,6 @@ namespace _Test.Core
                 //var privateKey = @"MIIEpAIBAAKCAQEA0CJNhxLG2wOo57eF/Ornj7YXD6/eJoFq/Gc/KNm5EoVafv9Ug92jmUbPAPWIM9Ci07KaQd+J6T18VCO2WzFtYHV3uyu+cIUNSrZcBHXple5u3Z4/1le76x/DaMGCaGfRdtg6hoON33BV6+2Y09KcOqpFankMDUhOHGxej+w4s5Gz02/OPY1ubH8RN9+kSd7Cu/NBDnyV1losPC/aGXtV8IuVT4hF3E6AFywNNer4R59oUO+bKoDIwu89eTVoZR2haRgtryhq9VCDFVIOVXGup2w5CVHnOLJsvtMtqjKMO/wCbVYTmbtGaVQy2r7W2FT+tbCUdxH3sc0kLYXtVfwhtwIDAQABAoIBAQCsqCgkKwFnYgvV3Tp7auqZHvbWfpAM5UM5CvUsECElKha+T1Vu5of2ePTz2LsaMLNCZmDs0GF5aRYgPlfiIoiXghrG3Czo7pbuKYT/9kjFpbu2gLZ4OuOa0wipeA2USrtKmWlDeRJSDsBYLQugfJA5YlKfVrcWtaqGjaeMQOtwmabvcswIYDe1saiBCRXr7/Iv4KFgpVMfRYls1wzwaDv393RaUKmSjxmIRLW+k75jKdglkcGAK3RqLLs/CIYQ/mHWxWQsS2xf1SN+vK3CNNXsVNO000i7uVwS2e9Ou/P9wYhOsADs4Axf+sTPUnDCZvzGblmpqtUzD6ErAcC8VKtxAoGBAP8O1c1lNI9yp65SVMgCFx8HaOn9U+wruKWJ6O0VeiEaJLasgwF05E+nEImM0Q/Scq5EoCg6mh2aY95iOam2aBwVgah9qHnrEJ+wChclxGviNNfDm1zcAeltUO2yHD4dtFPEIl+yXD3KUwlQRgFyvo+XhA/HOwsDTySDFSnAKrk1AoGBANDnGYVO6gjFmshlljRIlLohJnqVmvmtE5IUn5NCMX+TNakzvwpRzrtLc+1ZSf1yBMosgn88poDdjkGHZL8ZXwtEsV/+v/zkFQW0aEtFs67cVUfktOKSjHG+evqMZlekEcQTXZz1UG4wulZ7RBqQxM0PybBk5Dys9m43claE3ni7AoGBANq6VCuiIOLrhlT+Eeq7sCxR5GzVbITaMaz0iaXXhzaf/uARLP+wyKJuOMZc1mRlKye7fkVBjCza2844Gg8qeDmtT9W4fSSgq07mXqDfKIUEJiDqhG+r1I/jyUUuOv4h5yT2zCuY/3WV7oPMLVzMlBL78qq9Rir5mYNMTnfRblIJAoGADUQ/6KlkT35NICDjcxqQ52knim1p1CVbstFAeRehERsGM2Kn5T3gxSA7kn0zJ7dP+o7tEquFX3WyjRLOIRy5XnvUT+ZbxvGtLBmS7gTVLmurts8dda4c4TRZlwPHlBVFU5BvR4KEwxqxGsDlSFKdTPCNvHgLzpalZ8Z5qmjxv/UCgYAC3J1BB4h14e3mFfQ8wAI2KJudL2zHbWX3EG6DG+PLAfBNmw4PyYRycfViXBGZvYTka355vmxDFjkSQea7yIDk0oiL/QNQPXQF9BRcHMBgopW4eaj81RqVWv8P8QCMH4u1cE6mD5KwT2DB1cXMslkFS6oCN85nokFJZ6Jt/O0Ucg==";
                 //var v = EncryptUtil.Rsa.Decrypt(value, RSAExtensions.RSAKeyType.Pkcs1, privateKey);
                 //Console.WriteLine(v);
-
-                //// Save
-                //var dataBytes = File.ReadAllBytes(@"C:\Users\DwGoing\Desktop\1.mp4");
-                //var builder = new StringBuilder();
-                //builder.Append($"mp4|{dataBytes.Length}|");
-                //builder.Append(dataBytes.ToBase64String());
-                //File.WriteAllText(@"C:\Users\DwGoing\Desktop\1", builder.ToString());
-                //// Road
-                //var fileText = File.ReadAllText(@"C:\Users\DwGoing\Desktop\1");
-                //var tmp = fileText.Split("|");
-                //File.WriteAllBytes($"C:\\Users\\DwGoing\\Desktop\\2.{tmp[0]}", tmp[2].FromBase64String());
-                //Console.WriteLine("Finished");
             }
             catch (Exception ex)
             {
