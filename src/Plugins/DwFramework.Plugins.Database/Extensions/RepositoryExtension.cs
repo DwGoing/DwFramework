1feb840599289a9255fd29d1e8a70a363b9792e1
DwGoing <jianghy1209@163.com>
2020-09-09 20:54:41 +0800

    ADD：添加Database插件

---
+++ src/Plugins/DwFramework.Plugins.Database/Extensions/RepositoryExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using DwFramework.Core;
+using DwFramework.Database;
+
+namespace DwFramework.Extensions.Database
+{
+    public static class RepositoryExtension
+    {
+        /// <summary>
+        /// 注册服务
+        /// </summary>
+        /// <param name="host"></param>
+        public static void RegisterRepositories(this ServiceHost host)
+        {
+            host.RegisterDatabaseService();
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                var types = assembly.GetTypes();
+                foreach (var type in types)
+                {
+                    if (type.BaseType != null && type.BaseType.ToString().Contains("BaseRepository"))
+                        host.RegisterType(type);
+                }
+            }
+        }
+    }
+}
