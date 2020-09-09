78ae5cccad04fbade3fd4d55b3254887b2389ece
DwGoing <jianghy1209@163.com>
2020-09-09 20:21:48 +0800

    MOD：合并GetRoot和GetConfig

--- src/Module/DwFramework.Core/Extensions/ConfigurationExtension.cs
+++ src/Module/DwFramework.Core/Extensions/ConfigurationExtension.cs
@@ -5,20 +5,17 @@ namespace DwFramework.Core
     public static class ConfigurationExtension
     {
         /// <summary>
-        /// 从根节点获取配置
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="configuration"></param>
-        /// <returns></returns>
-        public static T GetRoot<T>(this IConfiguration configuration) => configuration.Get<T>();
-
-        /// <summary>
-        /// 从子节点获取配置
+        /// 获取配置
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="configuration"></param>
         /// <param name="key"></param>
         /// <returns></returns>
-        public static T GetConfig<T>(this IConfiguration configuration, string key) => configuration.GetSection(key).Get<T>();
+        public static T GetConfig<T>(this IConfiguration configuration, string key = null)
+        {
+            var section = configuration;
+            if (!string.IsNullOrEmpty(key)) section = section.GetSection(key);
+            return section.Get<T>();
+        }
     }
 }
