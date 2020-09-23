63bff198dbcfe7ad9c149d301e23c222f73619e0
DwGoing <jianghy1209@163.com>
2020-09-23 17:00:35 +0800

    ADD：添加Database示例

--- example/DwFramework.Example.Database/Program.cs
+++ example/DwFramework.Example.Database/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
+using DwFramework.Core;
+using DwFramework.Database;
+
 namespace DwFramework.Example.Database
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var host = new ServiceHost();
+                host.AddJsonConfig("Database.json");
+                host.RegisterDatabaseService();
+                host.OnInitialized += provider =>
+                {
+                    var db = provider.GetService<DatabaseService>();
+                    var result = db.DbConnection.Queryable<Record>().ToArray();
+                };
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
