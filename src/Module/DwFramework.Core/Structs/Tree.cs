3f592b61ab67bdb83cea2e4e20c348e4ff1a0864
DwGoing <jianghy1209@163.com>
2020-09-27 11:09:53 +0800

    ADD：新增Tree数据结构

---
+++ src/Module/DwFramework.Core/Structs/Tree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DwFramework.Core
+{
+    public sealed class Tree<T>
+    {
+        public TwoWayNode<T> Root { get; private set; }
+
+        public Tree()
+        {
+
+        }
+
+        public void Add(T value)
+        {
+            var node = new TwoWayNode<T>(value: value);
+        }
+
+        private void Plant(TwoWayNode<T> node)
+        {
+
+        }
+    }
+}
