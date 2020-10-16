4fd33536368fe929efbfbb885c885817fd018da4
DwGoing <jianghy1209@163.com>
2020-10-16 17:44:20 +0800

    TEST：测试异步操作

--- src/_AppTest/Program.cs
+++ src/_AppTest/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Threading;
 using System.Collections.Generic;
+using SqlSugar;
 using DwFramework.Core;
 using DwFramework.Core.Extensions;
 using DwFramework.Core.Plugins;
@@ -10,6 +11,7 @@ using DwFramework.WebSocket;
 using DwFramework.Rpc;
 using DwFramework.Rpc.Plugins;
 using DwFramework.DataFlow;
+using DwFramework.Database;
 
 namespace _AppTest
 {
@@ -19,51 +21,65 @@ namespace _AppTest
         {
             try
             {
-                var filter = new BloomFilter(100000);
-                for (var i = 1; i < 5000; i++)
-                {
-                    filter.Add(IdentificationGenerater.RandomString(6));
-                }
-                filter.Add("dwgoing");
-                Console.WriteLine(filter.IsExist("dwgoing1"));
-                Console.ReadKey();
-
-                //var host = new ServiceHost();
-                //host.RegisterLog();
-                //#region WebAPI
-                ////host.RegisterWebAPIService<Startup>("WebAPI.json");
-                //#endregion
-                //#region WebSocket
-                ////host.RegisterWebSocketService("WebSocket.json");
-                ////host.OnInitialized += p =>
-                ////{
-                ////    var websocket = p.GetWebSocketService();
-                ////    websocket.OnReceive += (c, args) => Console.WriteLine(args.Message);
-                ////};
-                //#endregion
-                //#region Rpc
-                ////host.RegisterClusterImpl("Cluster.json");
-                ////host.RegisterRpcService("Rpc.json");
-                ////host.OnInitialized += p => p.GetClusterImpl().OnJoin += id => Console.WriteLine(id);
-                //#endregion
-                //host.RegisterDataFlowService();
+                var host = new ServiceHost();
+                host.RegisterLog();
+                host.RegisterDatabaseService("Database.json");
+                #region WebAPI
+                //host.RegisterWebAPIService<Startup>("WebAPI.json");
+                #endregion
+                #region WebSocket
+                //host.RegisterWebSocketService("WebSocket.json");
                 //host.OnInitialized += p =>
                 //{
-                //    var val = 1;
-                //    switch (val)
-                //    {
-                //        case 1:
-                //            goto A;
-                //    }
-                //A:
-                //    Console.WriteLine("a");
+                //    var websocket = p.GetWebSocketService();
+                //    websocket.OnReceive += (c, args) => Console.WriteLine(args.Message);
                 //};
-                //host.Run();
+                #endregion
+                #region Rpc
+                //host.RegisterClusterImpl("Cluster.json");
+                //host.RegisterRpcService("Rpc.json");
+                //host.OnInitialized += p => p.GetClusterImpl().OnJoin += id => Console.WriteLine(id);
+                #endregion
+                host.OnInitialized += p =>
+                {
+                    var db = p.GetDatabaseService();
+                    var connection = db.DbConnection;
+                    connection.UseTran(() =>
+                    {
+                        var a = connection.Insertable(new T1()).ExecuteReturnEntity();
+                        D(connection);
+                    });
+                };
+                host.Run();
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
             }
         }
+
+        static void D(SqlSugarClient connection)
+        {
+            throw new Exception();
+            connection.Insertable(new T2() { }).ExecuteCommand();
+        }
+    }
+
+    [SugarTable("test1")]
+    public class T1
+    {
+        [SugarColumn(ColumnName = "id", IsIdentity = true, IsPrimaryKey = true)]
+        public int ID { get; set; }
+        [SugarColumn(ColumnName = "val")]
+        public string Value { get; set; }
+    }
+
+    [SugarTable("test2")]
+    public class T2
+    {
+        [SugarColumn(ColumnName = "id", IsIdentity = true, IsPrimaryKey = true)]
+        public int ID { get; set; }
+        [SugarColumn(ColumnName = "val")]
+        public string Value { get; set; }
     }
 }
