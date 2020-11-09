800bb81af58df727d3fb20f0457950223b1cd0fc
DwGoing <jianghy1209@163.com>
2020-11-09 15:53:56 +0800

    MOD：优化代码

--- src/Module/DwFramework.Core/Models/Enums.cs
+++ src/Module/DwFramework.Core/Models/Enums.cs
@@ -18,4 +18,15 @@
         Singleton,
         InstancePerLifetimeScope
     }
+
+    /// <summary>
+    /// 压缩类型
+    /// </summary>
+    public enum CompressType
+    {
+        Unknow = 0,
+        Brotli = 1,
+        GZip = 2,
+        LZ4 = 3
+    }
 }
