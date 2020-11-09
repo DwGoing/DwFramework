6c558089cf25efa2c4c190c8dcdf2b91992d600a
DwGoing <jianghy1209@163.com>
2020-11-09 14:47:09 +0800

    MOD：调整Object扩展

--- src/Module/DwFramework.Core/Extensions/ObjectExtension.cs
+++ src/Module/DwFramework.Core/Extensions/ObjectExtension.cs
@@ -12,15 +12,32 @@ namespace DwFramework.Core.Extensions
         /// <summary>
         /// 类型转换
         /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static object ConvertTo(this object obj, Type type)
+        {
+            var value = Convert.ChangeType(obj, type);
+            if (value == null) return default;
+            return value;
+        }
+
+        /// <summary>
+        /// 类型转换
+        /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="obj"></param>
         /// <returns></returns>
-        public static T ConvertTo<T>(this object obj)
-        {
-            var value = Convert.ChangeType(obj, typeof(T));
-            if (value == null) return default;
-            return (T)value;
-        }
+        public static T ConvertTo<T>(this object obj) => (T)obj.ConvertTo(typeof(T));
+
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="type"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public static string ToJson(this object obj, Type type, JsonSerializerOptions options = null) => JsonSerializer.Serialize(obj, type, options);
 
         /// <summary>
         /// 序列化
@@ -30,6 +47,15 @@ namespace DwFramework.Core.Extensions
         /// <param name="options"></param>
         /// <returns></returns>
         public static string ToJson<T>(this T obj, JsonSerializerOptions options = null) => JsonSerializer.Serialize(obj, options);
+
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <param name="json"></param>
+        /// <param name="type"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public static object ToObject(this string json, Type type, JsonSerializerOptions options = null) => JsonSerializer.Deserialize(json, type, options);
 
         /// <summary>
         /// 反序列化
@@ -58,6 +84,28 @@ namespace DwFramework.Core.Extensions
                 IFormatter formatter = new BinaryFormatter();
                 formatter.Serialize(stream, obj);
                 return stream.GetBuffer();
+            }
+        }
+
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="type"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static object ToObject(this byte[] bytes, Type type, Encoding encoding = null)
+        {
+            if (encoding != null)
+            {
+                var json = encoding.GetString(bytes);
+                return json.ToObject(type);
+            }
+            else
+            {
+                using var stream = new MemoryStream(bytes);
+                IFormatter formatter = new BinaryFormatter();
+                return formatter.Deserialize(stream);
             }
         }
 
