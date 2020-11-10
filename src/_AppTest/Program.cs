b06d08d3bcb49acd25b572548d5947d7dc9cf4dd
DwGoing <jianghy1209@163.com>
2020-11-10 10:44:14 +0800

    MOD：调整RPC服务注册方式

--- src/_AppTest/Program.cs
+++ src/_AppTest/Program.cs
@@ -7,24 +7,41 @@ using SqlSugar;
 using DwFramework.Core;
 using DwFramework.Core.Extensions;
 using DwFramework.Core.Plugins;
-using DwFramework.WebAPI;
-using DwFramework.WebSocket;
-using DwFramework.Rpc;
-using DwFramework.Rpc.Plugins;
+using DwFramework.RPC;
 using DwFramework.ORM;
 using DwFramework.ORM.Plugins;
+using System.Threading.Tasks;
+using Grpc.Core;
 
 namespace _AppTest
 {
     class Program
     {
+        public class AService : A.ABase
+        {
+            public override Task<Response> Do(Request request, ServerCallContext context)
+            {
+                return Task.FromResult(new Response()
+                {
+                    Message = request.Message
+                });
+            }
+        }
+
         static void Main(string[] args)
         {
             try
             {
                 var host = new ServiceHost();
+                host.RegisterType<AService>();
+                host.OnInitializing += p => p.GetRPCService().AddService(p.GetService<AService>());
+                host.RegisterRPCService("RPC.json");
                 host.OnInitialized += p =>
                 {
+                    var channel = new Channel("localhost:5020", ChannelCredentials.Insecure);
+                    var client = new A.AClient(channel);
+                    Console.WriteLine(client.Do(new Request() { Message = "123" }).Message);
+                    channel.ShutdownAsync();
                 };
                 host.Run();
             }
