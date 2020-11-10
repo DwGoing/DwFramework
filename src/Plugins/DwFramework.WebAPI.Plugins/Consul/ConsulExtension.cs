90018c6d4174e279f1763bc61cdaa59e08a1c162
DwGoing <jianghy1209@163.com>
2020-11-10 16:01:13 +0800

    MOD：变更项目名称

---
+++ src/Plugins/DwFramework.WebAPI.Plugins/Consul/ConsulExtension.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using Consul;
+
+using DwFramework.Core;
+
+namespace DwFramework.WebAPI.Plugins
+{
+    public static class ConsulExtension
+    {
+        public class Config
+        {
+            public class Service
+            {
+                public string Name { get; set; }
+                public int Port { get; set; }
+            }
+
+            public string Host { get; set; }
+            public string ServiceHost { get; set; }
+            public int HealthCheckPort { get; set; }
+            public string HealthCheckPath { get; set; } = "/consul/healthcheck";
+            public Service[] Services { get; set; }
+        }
+
+        /// <summary>
+        /// 注册服务
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="lifetime"></param>
+        /// <param name="configuration"></param>
+        /// <param name="configKey"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseConsul(this IApplicationBuilder app, IHostApplicationLifetime lifetime, IConfiguration configuration, string configKey = null)
+        {
+            var config = configuration.GetConfig<Config>(configKey);
+            foreach (var item in config.Services)
+            {
+                var client = new ConsulClient(c =>
+                {
+                    c.Address = new Uri(config.Host);
+                });
+                var registration = new AgentServiceRegistration()
+                {
+                    ID = $"{item.Name}-{config.ServiceHost}:{item.Port}",
+                    Name = item.Name,
+                    Address = config.ServiceHost,
+                    Port = item.Port,
+                    Check = new AgentServiceCheck()
+                    {
+                        DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(10),
+                        Interval = TimeSpan.FromSeconds(10),
+                        Timeout = TimeSpan.FromSeconds(5),
+                        HTTP = $"http://{config.ServiceHost}:{config.HealthCheckPort}{config.HealthCheckPath}"
+                    }
+                };
+                client.Agent.ServiceRegister(registration).Wait();
+                lifetime.ApplicationStopping.Register(() =>
+                {
+                    client.Agent.ServiceDeregister(registration.ID).Wait();
+                });
+            }
+            app.Map(config.HealthCheckPath, s => s.Run(context => context.Response.WriteAsync("ok")));
+            return app;
+        }
+
+        /// <summary>
+        /// 执行Consul操作
+        /// </summary>
+        /// <param name="consulHost"></param>
+        /// <param name="action"></param>
+        /// <param name="token"></param>
+        private static void Execute(string consulHost, Action<ConsulClient> action, string token = null)
+        {
+            action(new ConsulClient(c =>
+            {
+                c.Address = new Uri(consulHost);
+                c.Token = token;
+            }));
+        }
+
+        /// <summary>
+        /// 获取服务
+        /// </summary>
+        /// <param name="consulHost"></param>
+        /// <param name="serviceNames"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static AgentService[] GetServices(string consulHost, string[] serviceNames = null, string token = null)
+        {
+            AgentService[] services = null;
+            Execute(consulHost, client =>
+            {
+                services = client.Agent.Services().Result.Response.Values.ToArray();
+                if (serviceNames != null) services = services.Where(item => serviceNames.Contains(item.Service)).ToArray();
+            }, token);
+            return services;
+        }
+    }
+}
---
+++ src/Plugins/DwFramework.WebAPI.Plugins/Jwt/JwtManager.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+using DwFramework.Core.Plugins;
+
+namespace DwFramework.WebAPI.Plugins
+{
+    public static class JwtManager
+    {
+        public static string Tag { get; private set; }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        static JwtManager()
+        {
+            Tag = RandomGenerater.RandomString(16);
+        }
+
+        /// <summary>
+        /// 注入JWT服务
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="tokenValidator"></param>
+        /// <param name="onSuccess"></param>
+        /// <param name="onFail"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, ISecurityTokenValidator tokenValidator, Func<TokenValidatedContext, Task> onSuccess, Func<JwtBearerChallengeContext, Task> onFail)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.SecurityTokenValidators.Clear();
+                    options.SecurityTokenValidators.Add(tokenValidator);
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnTokenValidated = onSuccess,
+                        OnChallenge = onFail
+                    };
+                });
+            return services;
+        }
+
+        /// <summary>
+        /// 生成Token
+        /// </summary>
+        /// <param name="issuer"></param>
+        /// <param name="securityKey"></param>
+        /// <param name="audiences"></param>
+        /// <param name="notBefore"></param>
+        /// <param name="expires"></param>
+        /// <param name="customFields"></param>
+        /// <returns></returns>
+        public static string GenerateToken(string issuer, string securityKey, string[] audiences = null, DateTime? notBefore = null, DateTime? expires = null, Dictionary<string, object> customFields = null)
+        {
+            if (securityKey.Length < 16) throw new Exception("SecuriyKey长度不足");
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            var claims = new List<Claim>();
+            if (audiences != null) foreach (var item in audiences) claims.Add(new Claim("aud", item));
+            var creds = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey)), SecurityAlgorithms.HmacSha256);
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: issuer,
+                claims: claims,
+                notBefore: notBefore,
+                expires: expires,
+                signingCredentials: creds
+                );
+            // 自定义内容
+            jwtSecurityToken.Header["tag"] = Tag;
+            if (customFields != null) foreach (var keyValuePair in customFields) jwtSecurityToken.Payload[keyValuePair.Key] = keyValuePair.Value;
+            var token = tokenHandler.WriteToken(jwtSecurityToken);
+            return token;
+        }
+
+        /// <summary>
+        /// 刷新Token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="securityKey"></param>
+        /// <param name="notBefore"></param>
+        /// <param name="expires"></param>
+        /// <returns></returns>
+        public static string RefreshToken(string token, string securityKey, DateTime? notBefore = null, DateTime? expires = null)
+        {
+            // 验证
+            ValidateSecurityKey(token, securityKey);
+            var sourceToken = DecodeToken(token);
+            // 生成新Token
+            return GenerateToken(sourceToken.Issuer, securityKey, sourceToken.Audiences.ToArray(), notBefore, expires, ReadClaims(token));
+        }
+
+        /// <summary>
+        /// 验证SecurityKey及Lifetime
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="securityKey"></param>
+        /// <param name="validateLifetime"></param>
+        private static void ValidateSecurityKey(string token, string securityKey, bool validateLifetime = false)
+        {
+            var parameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = validateLifetime,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey))
+            };
+            new JwtSecurityTokenHandler().ValidateToken(token, parameters, out _);
+        }
+
+        /// <summary>
+        /// 解析Token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static JwtSecurityToken DecodeToken(string token)
+        {
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            if (string.IsNullOrEmpty(token) || !tokenHandler.CanReadToken(token))
+                throw new Exception("无效的Token");
+            return tokenHandler.ReadJwtToken(token);
+        }
+
+        /// <summary>
+        /// 读取Claims信息
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static Dictionary<string, object> ReadClaims(string token)
+        {
+            var jwt = DecodeToken(token);
+            return jwt.Payload.ToDictionary(item => item.Key, item => item.Value);
+        }
+
+        /// <summary>
+        /// 读取Claims信息
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static object ReadClaim(string token, string key)
+        {
+            var jwt = DecodeToken(token);
+            return jwt.Payload[key];
+        }
+    }
+}
