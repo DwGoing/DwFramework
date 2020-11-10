b8722913060f9d78dd8932d8a8535cf63a0978cf
DwGoing <jianghy1209@163.com>
2020-11-10 17:01:34 +0800

    DEL：移除发送string消息

--- src/_AppTest/Program.cs
+++ src/_AppTest/Program.cs
@@ -9,6 +9,7 @@ using DwFramework.RPC;
 using DwFramework.RPC.Plugins;
 using DwFramework.Socket;
 using DwFramework.WebAPI;
+using DwFramework.WebSocket;
 
 namespace _AppTest
 {
@@ -21,6 +22,30 @@ namespace _AppTest
                 var host = new ServiceHost();
                 host.RegisterLog();
                 host.RegisterWebAPIService<Startup>("WebAPI.json");
+                host.RegisterWebSocketService("WebSocket.json");
+                host.OnInitializing += provider =>
+                {
+                    var service = provider.GetWebSocketService();
+                    service.OnConnect += (c, a) =>
+                    {
+                        Console.WriteLine($"{c.ID}已连接");
+                    };
+                    service.OnSend += (c, a) =>
+                    {
+                        var msg = Encoding.UTF8.GetString(a.Data);
+                        Console.WriteLine($"向{c.ID}发送消息：{msg}");
+                    };
+                    service.OnReceive += (c, a) =>
+                    {
+                        var msg = Encoding.UTF8.GetString(a.Data);
+                        Console.WriteLine($"收到{c.ID}发来的消息：{msg}");
+                        service.SendAsync(c.ID, a.Data);
+                    };
+                    service.OnClose += (c, a) =>
+                    {
+                        Console.WriteLine($"{c.ID}已断开");
+                    };
+                };
                 host.Run();
             }
             catch (Exception ex)
