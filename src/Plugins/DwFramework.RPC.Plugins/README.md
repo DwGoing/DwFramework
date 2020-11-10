c8caded43950e795da8a509a116cf18b397d7b93
DwGoing <jianghy1209@163.com>
2020-11-10 14:58:39 +0800

    FIX：修复无法传输消息的异常

--- src/Plugins/DwFramework.RPC.Plugins/README.md
+++ src/Plugins/DwFramework.RPC.Plugins/README.md
@@ -12,16 +12,26 @@ PM> Install-Package DwFramework.Plugins.Rpc
 
 该插件基于RPC服务实现，满足服务实现去中心化的集群实现。
 
+```json
+{
+  "LinkUrl": "{自身RPC IP:Port}",
+  "BootPeer": "{启动连接节点}"
+}
+```
+
 ```c#
 // 注册集群服务
-host.RegisterClusterImpl({本地服务连接URL}, {启动连接节点});
+host.RegisterClusterImpl({配置文件路径});
 host.RegisterRpcService(); // 一定要在RegisterClusterImpl之后
 host.OnInitializing += p =>
 {
-  var cluster = p.GetClusterImpl();
-  // SyncData() 在集群中同步数据
-  cluster.OnJoin += id => cluster.SyncData(Encoding.UTF8.GetBytes($"欢迎 {id} 加入集群"));
-  cluster.OnReceiveData += (id, data) => Console.WriteLine($"收到 {id} 消息:{Encoding.UTF8.GetString(data)}");
+    var cluster = p.GetClusterImpl();
+    cluster.OnJoin += id => Console.WriteLine($"欢迎 {id} 加入集群");
+    cluster.OnExit += id => Console.WriteLine($"{id} 退出集群");
+    // SyncData() 在集群中同步数据
+    cluster.OnJoin += id => cluster.SyncData(DataType.Text, Encoding.UTF8.GetBytes($"欢迎 {id} 加入集群"));
+    cluster.OnReceiveData += (id, type, data) => Console.WriteLine($"收到 {id} 消息:{data.ToObject<string>(Encoding.UTF8)}");
+    cluster.OnConnectBootPeerFailed += ex => Console.WriteLine(ex);
 };
 ```
 
--- src/_AppTest/Program.cs
+++ src/_AppTest/Program.cs
@@ -8,6 +8,7 @@ using DwFramework.Core;
 using DwFramework.Core.Extensions;
 using DwFramework.Core.Plugins;
 using DwFramework.RPC;
+using DwFramework.RPC.Plugins;
 using DwFramework.ORM;
 using DwFramework.ORM.Plugins;
 using System.Threading.Tasks;
@@ -17,31 +18,22 @@ namespace _AppTest
 {
     class Program
     {
-        public class AService : A.ABase
-        {
-            public override Task<Response> Do(Request request, ServerCallContext context)
-            {
-                return Task.FromResult(new Response()
-                {
-                    Message = request.Message
-                });
-            }
-        }
-
         static void Main(string[] args)
         {
             try
             {
                 var host = new ServiceHost();
-                host.RegisterType<AService>();
-                host.OnInitializing += p => p.GetRPCService().AddService(p.GetService<AService>());
+                host.RegisterClusterImpl("Cluster.json");
                 host.RegisterRPCService("RPC.json");
-                host.OnInitialized += p =>
+                host.OnInitializing += p =>
                 {
-                    var channel = new Channel("localhost:5020", ChannelCredentials.Insecure);
-                    var client = new A.AClient(channel);
-                    Console.WriteLine(client.Do(new Request() { Message = "123" }).Message);
-                    channel.ShutdownAsync();
+                    var cluster = p.GetClusterImpl();
+                    cluster.OnJoin += id => Console.WriteLine($"欢迎 {id} 加入集群");
+                    cluster.OnExit += id => Console.WriteLine($"{id} 退出集群");
+                    // SyncData() 在集群中同步数据
+                    cluster.OnJoin += id => cluster.SyncData(DataType.Text, $"欢迎 {id} 加入集群".ToBytes(Encoding.UTF8));
+                    cluster.OnReceiveData += (id, type, data) => Console.WriteLine($"收到 {id} 消息:{data.ToObject<string>(Encoding.UTF8)}");
+                    cluster.OnConnectBootPeerFailed += ex => Console.WriteLine(ex);
                 };
                 host.Run();
             }
