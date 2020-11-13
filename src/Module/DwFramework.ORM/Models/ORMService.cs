b691407aeb275e1d651ec47449f126f3a4706ea6
DwGoing <jianghy1209@163.com>
2020-11-13 09:41:07 +0800

    MOD：调整连接获取方式

--- src/Module/DwFramework.ORM/Models/ORMService.cs
+++ src/Module/DwFramework.ORM/Models/ORMService.cs
@@ -37,6 +37,8 @@ namespace DwFramework.ORM
 
         private readonly Config _config;
 
+        public SqlSugarClient DbConnection => CreateConnection();
+
         /// <summary>
         /// 构造函数
         /// </summary>
@@ -90,21 +92,21 @@ namespace DwFramework.ORM
         /// <param name="columns"></param>
         /// <param name="isCreatePrimaryKey"></param>
         /// <returns></returns>
-        public Task<bool> CreateTableAsync(string tableName, List<DbColumnInfo> columns, bool isCreatePrimaryKey = true) => TaskManager.CreateTask(() => CreateConnection().DbMaintenance.CreateTable(tableName, columns, isCreatePrimaryKey));
+        public Task<bool> CreateTableAsync(string tableName, List<DbColumnInfo> columns, bool isCreatePrimaryKey = true) => TaskManager.CreateTask(() => DbConnection.DbMaintenance.CreateTable(tableName, columns, isCreatePrimaryKey));
 
         /// <summary>
         /// 删除表
         /// </summary>
         /// <param name="tableName"></param>
         /// <returns></returns>
-        public Task<bool> DropTableAsync(string tableName) => TaskManager.CreateTask(() => CreateConnection().DbMaintenance.DropTable(tableName));
+        public Task<bool> DropTableAsync(string tableName) => TaskManager.CreateTask(() => DbConnection.DbMaintenance.DropTable(tableName));
 
         /// <summary>
         /// 重置表
         /// </summary>
         /// <param name="tableName"></param>
         /// <returns></returns>
-        public Task<bool> TruncateTableAsync(string tableName) => TaskManager.CreateTask(() => CreateConnection().DbMaintenance.TruncateTable(tableName));
+        public Task<bool> TruncateTableAsync(string tableName) => TaskManager.CreateTask(() => DbConnection.DbMaintenance.TruncateTable(tableName));
 
         /// <summary>
         /// 复制表结构
@@ -116,7 +118,7 @@ namespace DwFramework.ORM
         {
             return TaskManager.CreateTask(() =>
             {
-                var connection = CreateConnection();
+                var connection = DbConnection;
                 var columns = connection.DbMaintenance.GetColumnInfosByTableName(from);
                 return connection.DbMaintenance.CreateTable(to, columns);
             });
