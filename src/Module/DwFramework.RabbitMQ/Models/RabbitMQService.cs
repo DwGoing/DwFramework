19fb2069e4248d0f5ba53a5558f381590ef7b275
DwGoing <jianghy1209@163.com>
2020-11-18 11:35:30 +0800

    FIX：编码失效

--- src/Module/DwFramework.RabbitMQ/Models/RabbitMQService.cs
+++ src/Module/DwFramework.RabbitMQ/Models/RabbitMQService.cs
@@ -142,7 +142,7 @@ namespace DwFramework.RabbitMQ
                 basicProperties = channel.CreateBasicProperties();
                 basicPropertiesSetting(basicProperties);
             }
-            var body = encoding != null ? Encoding.UTF8.GetBytes(data.ToJson()) : data.ToBytes();
+            var body = encoding != null ? encoding.GetBytes(data.ToJson()) : data.ToBytes();
             if (returnAction != null) channel.BasicReturn += (sender, args) => returnAction(args);
             channel.BasicPublish(exchange, routingKey, basicProperties, body);
         }
@@ -182,7 +182,7 @@ namespace DwFramework.RabbitMQ
                 basicProperties = channel.CreateBasicProperties();
                 basicPropertiesSetting(basicProperties);
             }
-            var body = encoding != null ? Encoding.UTF8.GetBytes(data.ToJson()) : data.ToBytes();
+            var body = encoding != null ? encoding.GetBytes(data.ToJson()) : data.ToBytes();
             if (returnAction != null) channel.BasicReturn += (sender, args) => returnAction(args);
             channel.BasicPublish(exchange, routingKey, basicProperties, body);
             if (timeoutSeconds >= 0) return channel.WaitForConfirms(TimeSpan.FromSeconds(timeoutSeconds));
