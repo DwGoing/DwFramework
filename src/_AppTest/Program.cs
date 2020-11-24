12818a554656537d0237d55dfd38215cc9aa7f87
DwGoing <jianghy1209@163.com>
2020-11-24 17:55:06 +0800

    MOD：修改测试代码

--- src/_AppTest/Program.cs
+++ src/_AppTest/Program.cs
@@ -18,66 +18,24 @@ namespace _AppTest
 {
     class Program
     {
-        static object o = new object();
+        class Obj
+        {
+            public int A { get; set; }
+            public string B { get; set; }
+        }
 
         static void Main(string[] args)
         {
             try
             {
-                var hash = new HashSet<long>();
-                var g1 = new SnowflakeGenerater(1);
-                var g2 = new SnowflakeGenerater(2);
-                var g3 = new SnowflakeGenerater(3);
-                Task.Run(() =>
+                var host = new ServiceHost();
+                host.RegisterLog();
+                host.AddJsonConfig("Config.json");
+                host.OnInitialized += p =>
                 {
-                    while (true)
-                    {
-                        lock (o)
-                        {
-                            var id = g1.GenerateId();
-                            if (hash.Contains(id)) Console.WriteLine($"{id} !!!");
-                            else { hash.Add(id); Console.WriteLine($"{id}"); }
-                        }
-                        Thread.Sleep(1);
-                    }
-                });
-                Task.Run(() =>
-                {
-                    while (true)
-                    {
-                        lock (o)
-                        {
-                            var id = g2.GenerateId();
-                            if (hash.Contains(id)) Console.WriteLine($"{id} !!!");
-                            else { hash.Add(id); Console.WriteLine($"{id}"); }
-                        }
-                        Thread.Sleep(1);
-                    }
-                });
-                Task.Run(() =>
-                {
-                    while (true)
-                    {
-                        lock (o)
-                        {
-                            var id = g3.GenerateId();
-                            if (hash.Contains(id)) Console.WriteLine($"{id} !!!");
-                            else { hash.Add(id); Console.WriteLine($"{id}"); }
-                        }
-                        Thread.Sleep(1);
-                    }
-                });
-
-                // var host = new ServiceHost();
-                // host.RegisterLog();
-                // host.AddJsonConfig("ORM.json");
-                // host.RegisterType<ORMService>();
-                // host.OnInitialized += p =>
-                // {
-                //     var s = p.GetORMService();
-                //     var r= s.CreateConnection("DB2").Queryable<dynamic>().AS("zsy").ToDataTable();
-                // };
-                // host.Run();
+                    var config = p.GetService<DwFramework.Core.Environment>().GetConfiguration().GetConfig<Obj[]>("RawInfos");
+                };
+                host.Run();
             }
             catch (Exception ex)
             {
