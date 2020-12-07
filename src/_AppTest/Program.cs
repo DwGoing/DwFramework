cc5dc7d45d3a2e3c38c08666e1be8eafe7c70ecf
DwGoing <jianghy1209@163.com>
2020-12-07 18:56:09 +0800

    UPD：更新文档

--- src/_AppTest/Program.cs
+++ src/_AppTest/Program.cs
@@ -26,38 +26,8 @@ namespace _AppTest
             {
                 var host = new ServiceHost();
                 host.RegisterLog();
-                host.AddJsonConfig("RabbitMQ.json");
-                host.RegisterRabbitMQService();
-                host.OnInitialized += p =>
-                {
-                    try
-                    {
-                        var mq = p.GetRabbitMQService();
-                        //mq.Subscribe("queue.indexservice.datapush.DiagnosisAndTreatment", false, (m, a) =>
-                        //{
-                        //    Console.WriteLine("DiagnosisAndTreatment");
-                        //    Thread.Sleep(200);
-                        //    m.BasicAck(a.DeliveryTag, false);
-                        //}, 2, 5);
-                        //mq.Subscribe("queue.indexservice.datapush.FamilyMedicine", false, (m, a) =>
-                        //{
-                        //    Console.WriteLine("FamilyMedicine");
-                        //    Thread.Sleep(200);
-                        //    m.BasicAck(a.DeliveryTag, false);
-                        //}, 2, 5);
-                        mq.Subscribe("queue.indexservice.datapush.PublicHealth", false, (m, a) =>
-                        {
-                            var i = new Random().Next(50, 200);
-                            Console.WriteLine(i + "ms");
-                            Thread.Sleep(i);
-                            m.BasicAck(a.DeliveryTag, false);
-                        }, 3, 10);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex);
-                    }
-                };
+                host.AddJsonConfig("RPC.json");
+                host.RegisterRPCService();
                 host.Run();
             }
             catch (Exception ex)
