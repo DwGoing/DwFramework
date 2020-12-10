70f83f698db79b701ab213009f834282f0d4a2b7
DwGoing <jianghy1209@163.com>
2020-12-10 11:13:16 +0800

    ADD：新增Delegate扩展

---
+++ src/Module/DwFramework.Core/Extensions/DelegateExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DwFramework.Core.Extensions
+{
+    public static class DelegateExtension
+    {
+        public static void ClearAll(this MulticastDelegate @delegate)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+    }
+}
