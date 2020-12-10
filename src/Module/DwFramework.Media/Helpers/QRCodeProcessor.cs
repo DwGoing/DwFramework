9acc71e41bb7fb1fa7d627ab210e7d912ac54a60
DwGoing <jianghy1209@163.com>
2020-12-10 11:12:53 +0800

    ADD：新增Media库

---
+++ src/Module/DwFramework.Media/Helpers/QRCodeProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using OpenCvSharp;
+
+namespace DwFramework.Media
+{
+    public static class QRCodeProcessor
+    {
+        /// <summary>
+        /// 解析二维码
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string Decode(string path)
+        {
+            var src = new ImageData(path);
+            var detector = new QRCodeDetector();
+            return detector.DetectAndDecode(src.ToMat(), out var _);
+        }
+
+        /// <summary>
+        /// 解析二维码
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static string Decode(Stream stream)
+        {
+            var src = new ImageData(stream);
+            var detector = new QRCodeDetector();
+            return detector.DetectAndDecode(src.ToMat(), out var _);
+        }
+    }
+}
