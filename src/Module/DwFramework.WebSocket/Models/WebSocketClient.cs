fdf36e19e4a4868c73b688b88d53e2a90e417992
DwGoing <jianghy1209@163.com>
2020-12-10 13:35:13 +0800

    FIX：主动断开连接时的异常

--- src/Module/DwFramework.WebSocket/Models/WebSocketClient.cs
+++ src/Module/DwFramework.WebSocket/Models/WebSocketClient.cs
@@ -2,7 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using System.Net.WebSockets;
-using System.Text;
+using System.Linq;
 using System.Collections.Generic;
 
 using DwFramework.Core.Extensions;
@@ -19,21 +19,21 @@ namespace DwFramework.WebSocket
 
         public class OnSendEventargs : EventArgs
         {
-            public string Message { get; private set; }
+            public byte[] Data { get; }
 
-            public OnSendEventargs(string msg)
+            public OnSendEventargs(byte[] data)
             {
-                Message = msg;
+                Data = data;
             }
         }
 
         public class OnReceiveEventargs : EventArgs
         {
-            public string Message { get; private set; }
+            public byte[] Data { get; }
 
-            public OnReceiveEventargs(string msg)
+            public OnReceiveEventargs(byte[] data)
             {
-                Message = msg;
+                Data = data;
             }
         }
 
@@ -44,7 +44,7 @@ namespace DwFramework.WebSocket
 
         public class OnErrorEventargs : EventArgs
         {
-            public Exception Exception { get; private set; }
+            public Exception Exception { get; }
 
             public OnErrorEventargs(Exception exception)
             {
@@ -53,9 +53,9 @@ namespace DwFramework.WebSocket
         }
 
         public event Action<OnConnectEventargs> OnConnect;
+        public event Action<OnCloceEventargs> OnClose;
         public event Action<OnSendEventargs> OnSend;
         public event Action<OnReceiveEventargs> OnReceive;
-        public event Action<OnCloceEventargs> OnClose;
         public event Action<OnErrorEventargs> OnError;
 
         private readonly ClientWebSocket _client;
@@ -89,36 +89,37 @@ namespace DwFramework.WebSocket
         /// <param name="header"></param>
         /// <param name="subProtocal"></param>
         /// <returns></returns>
-        public Task ConnectAsync(string uri, Dictionary<string, string> header = null, List<string> subProtocal = null)
+        public async Task ConnectAsync(string uri, Dictionary<string, string> header = null, List<string> subProtocal = null)
         {
             if (header != null) foreach (var item in header) _client.Options.SetRequestHeader(item.Key, item.Value);
             if (subProtocal != null) foreach (var item in subProtocal) _client.Options.AddSubProtocol(item);
-            return _client.ConnectAsync(new Uri(uri), CancellationToken.None).ContinueWith(a =>
+            await _client.ConnectAsync(new Uri(uri), CancellationToken.None).ContinueWith(a =>
             {
                 OnConnect?.Invoke(new OnConnectEventargs() { });
                 TaskManager.CreateTask(async () =>
                 {
                     var buffer = new byte[_bufferSize];
-                    var result = await _client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                    while (!result.CloseStatus.HasValue)
+                    var dataBytes = new List<byte>();
+                    while (true)
                     {
                         try
                         {
-                            var msg = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                            OnReceive?.Invoke(new OnReceiveEventargs(msg));
+                            var result = await _client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                            if (result.CloseStatus.HasValue) break;
+                            dataBytes.AddRange(buffer.Take(result.Count));
+                            if (!result.EndOfMessage) continue;
+                            OnReceive?.Invoke(new OnReceiveEventargs(dataBytes.ToArray()));
+                            dataBytes.Clear();
                         }
                         catch (Exception ex)
                         {
                             OnError?.Invoke(new OnErrorEventargs(ex));
-                        }
-                        finally
-                        {
-                            buffer = new byte[_bufferSize];
-                            result = await _client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                            break;
                         }
                     }
                     OnClose?.Invoke(new OnCloceEventargs() { });
                     ClearAllEvent();
+                    if (_client.State == WebSocketState.CloseReceived) await CloseAsync();
                 });
             });
         }
@@ -128,29 +129,22 @@ namespace DwFramework.WebSocket
         /// </summary>
         /// <param name="buffer"></param>
         /// <returns></returns>
-        public Task SendAsync(byte[] buffer)
+        public async Task SendAsync(byte[] buffer)
         {
-            return _client.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None)
-                .ContinueWith(a => OnSend?.Invoke(new OnSendEventargs(Encoding.UTF8.GetString(buffer)) { }));
-        }
-
-        /// <summary>
-        /// 发送消息
-        /// </summary>
-        /// <param name="msg"></param>
-        /// <returns></returns>
-        public Task SendAsync(string msg)
-        {
-            return SendAsync(Encoding.UTF8.GetBytes(msg));
+            await _client.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None).ContinueWith(task =>
+            {
+                if (task.IsCompletedSuccessfully) OnSend?.Invoke(new OnSendEventargs(buffer));
+                else OnError?.Invoke(new OnErrorEventargs(task.Exception.InnerException));
+            });
         }
 
         /// <summary>
         /// 断开连接
         /// </summary>
         /// <returns></returns>
-        public Task CloseAsync()
+        public async Task CloseAsync()
         {
-            return _client.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
+            await _client.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
         }
 
         /// <summary>
