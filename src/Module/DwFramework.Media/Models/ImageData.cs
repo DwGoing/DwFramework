6cf152827232815a9af922327fce2b9ad1f2d948
DwGoing <jianghy1209@163.com>
2020-12-15 14:08:44 +0800

    TMP：临时提交

--- src/Module/DwFramework.Media/Models/ImageData.cs
+++ src/Module/DwFramework.Media/Models/ImageData.cs
@@ -1,25 +1,17 @@
 ﻿using System;
 using System.IO;
-using SkiaSharp;
+using System.Drawing;
 using OpenCvSharp;
+using OpenCvSharp.Extensions;
 
 namespace DwFramework.Media
 {
     public sealed class ImageData : IDisposable
     {
-        private SKBitmap _skBitmap;
+        private Stream _stream;
 
-        public int Width => _skBitmap == null ? 0 : _skBitmap.Width;
-        public int Height => _skBitmap == null ? 0 : _skBitmap.Height;
-
-        /// <summary>
-        /// 构造函数
-        /// </summary>
-        /// <param name="skBitmap"></param>
-        public ImageData(SKBitmap skBitmap)
-        {
-            _skBitmap = skBitmap;
-        }
+        public int Width => GetWidth();
+        public int Height => GetHeight();
 
         /// <summary>
         /// 构造函数
@@ -28,8 +20,17 @@ namespace DwFramework.Media
         public ImageData(Stream stream)
         {
             if (stream == null || stream.Length <= 0) throw new Exception("stream参数为空");
-            using var skStream = new SKManagedStream(stream, true);
-            _skBitmap = SKBitmap.Decode(skStream);
+            _stream = stream;
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="mat"></param>
+        public ImageData(Mat mat)
+        {
+            if (mat == null || mat.Empty()) throw new Exception("mat参数为空");
+            _stream = mat.ToMemoryStream();
         }
 
         /// <summary>
@@ -40,33 +41,17 @@ namespace DwFramework.Media
         {
             if (string.IsNullOrEmpty(path)) throw new Exception("path为空");
             if (!File.Exists(path)) throw new Exception("图片不存在");
-            using var stream = new FileStream(path, FileMode.Open, FileAccess.Read);
-            if (stream == null || stream.Length <= 0) throw new Exception("stream参数为空");
-            using var skStream = new SKManagedStream(stream, true);
-            _skBitmap = SKBitmap.Decode(skStream);
+            _stream = new FileStream(path, FileMode.Open, FileAccess.Read);
         }
 
         /// <summary>
-        /// 获取字节数组
+        /// 获取流
         /// </summary>
         /// <returns></returns>
-        private byte[] ToBytes()
+        public Stream ToStream()
         {
-            if (_skBitmap == null) return null;
-            var bytes = new byte[Width * Height * 3];
-            var byteIndex = 0;
-            for (var y = 0; y < Height; y++)
-            {
-                for (var x = 0; x < Width; x++)
-                {
-                    var color = _skBitmap.GetPixel(x, y);
-                    bytes[byteIndex + 0] = color.Blue;
-                    bytes[byteIndex + 1] = color.Green;
-                    bytes[byteIndex + 2] = color.Red;
-                    byteIndex += 3;
-                }
-            }
-            return bytes;
+            if (_stream == null || _stream.Length <= 0) throw new Exception("数据长度为0");
+            return _stream;
         }
 
         /// <summary>
@@ -75,7 +60,50 @@ namespace DwFramework.Media
         /// <returns></returns>
         public Mat ToMat()
         {
-            return new Mat(_skBitmap.Height, _skBitmap.Width, MatType.CV_8UC3, ToBytes());
+            if (_stream == null || _stream.Length <= 0) throw new Exception("数据长度为0");
+            return Mat.FromStream(_stream, ImreadModes.Unchanged);
+        }
+
+        /// <summary>
+        /// 获取字节数组
+        /// </summary>
+        /// <returns></returns>
+        public byte[] ToBytes()
+        {
+            if (_stream == null || _stream.Length <= 0) throw new Exception("数据长度为0");
+            using var mat = ToMat();
+            return mat.ToBytes();
+        }
+
+        /// <summary>
+        /// 获取Bitmap
+        /// </summary>
+        /// <returns></returns>
+        public Bitmap ToBitmap()
+        {
+            if (_stream == null || _stream.Length <= 0) throw new Exception("数据长度为0");
+            using var mat = ToMat();
+            return mat.ToBitmap();
+        }
+
+        /// <summary>
+        /// 获取宽度
+        /// </summary>
+        /// <returns></returns>
+        public int GetWidth()
+        {
+            using var mat = ToMat();
+            return mat.Width;
+        }
+
+        /// <summary>
+        /// 获取高度
+        /// </summary>
+        /// <returns></returns>
+        public int GetHeight()
+        {
+            using var mat = ToMat();
+            return mat.Height;
         }
 
         /// <summary>
@@ -83,8 +111,8 @@ namespace DwFramework.Media
         /// </summary>
         public void Dispose()
         {
-            _skBitmap.Dispose();
-            _skBitmap = null;
+            _stream.Dispose();
+            _stream = null;
         }
     }
 }
