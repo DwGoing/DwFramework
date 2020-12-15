cc4a509cd67ff99e578296af94c380c20c8675b2
DwGoing <jianghy1209@163.com>
2020-12-15 14:07:39 +0800

    ADD：增加图像识别处理器

---
+++ src/Module/DwFramework.Media/Helpers/ImageRecognitionProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using OpenCvSharp;
+
+namespace DwFramework.Media
+{
+    public static class ImageRecognitionProcessor
+    {
+        private static CascadeClassifier _classifier;
+
+        public static void Init(string xmlPath)
+        {
+            if (string.IsNullOrEmpty(xmlPath)) throw new Exception("xmlPath参数为空");
+            if (!File.Exists(xmlPath)) throw new Exception("文件不存在");
+            _classifier = new CascadeClassifier(xmlPath);
+        }
+
+        public static void Recognize()
+        {
+        }
+    }
+}
