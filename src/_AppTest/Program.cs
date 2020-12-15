3ecef1997d78c4294a789c1dea85ab6e3f7a9ea3
DwGoing <jianghy1209@163.com>
2020-12-15 14:11:54 +0800

    TMP：临时提交

--- src/_AppTest/Program.cs
+++ src/_AppTest/Program.cs
@@ -1,43 +1,14 @@
 ﻿using System;
-using System.Linq.Expressions;
-using System.Text;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using DwFramework.Core;
-using DwFramework.Core.Extensions;
-using DwFramework.Core.Plugins;
-using DwFramework.ORM;
-using DwFramework.ORM.Plugins;
-using DwFramework.RabbitMQ;
-using DwFramework.RPC;
-using DwFramework.RPC.Plugins;
-using DwFramework.Socket;
-using DwFramework.TaskSchedule;
-using DwFramework.WebAPI;
-using DwFramework.WebSocket;
-using Quartz;
-using Microsoft.Extensions.Logging;
 
 namespace _AppTest
 {
-    class AA
-    {
-        public int id { get; set; }
-        public string Name { get; set; }
-        public string IsOk { get; set; }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             try
             {
-                //var host = new ServiceHost(configFilePath: "WebAPI.json");
-                //host.RegisterLog();
-                //host.RegisterWebAPIService<Startup>();
-                //host.Run();
 
             }
             catch (Exception ex)
