72300db54de978e5e8c9e284828c52ac19e01e1a
DwGoing <jianghy1209@163.com>
2020-12-24 15:26:50 +0800

    ADD：增加身份证内容识别

--- src/_AppTest/Program.cs
+++ src/_AppTest/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Threading.Tasks;
 using DwFramework.Core;
+using DwFramework.Media;
+
+using static Tensorflow.Binding;
+using NumSharp;
+using OpenCvSharp;
 
 namespace _AppTest
 {
@@ -9,7 +15,12 @@ namespace _AppTest
         {
             try
             {
-
+                //// 矩阵乘法
+                //var matrix1 = tf.constant(np.array(new float[,] { { 1, 2 }, { 3, 4 } }));
+                //var matrix2 = tf.constant(np.array(new float[,] { { 5, 6 }, { 7, 8 } }));
+                //var product = tf.matmul(matrix1, matrix2);
+                //// 类型转换：tensor转换成numpy
+                //print("product =", product.numpy());
             }
             catch (Exception ex)
             {
