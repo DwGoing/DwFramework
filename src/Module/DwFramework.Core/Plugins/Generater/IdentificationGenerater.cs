2a6df25a360f5889e06069a22b319359f15f7c69
DwGoing <jianghy1209@163.com>
2020-12-29 16:11:23 +0800

    ADD：添加雪花ID解析

--- src/Module/DwFramework.Core/Plugins/Generater/IdentificationGenerater.cs
+++ src/Module/DwFramework.Core/Plugins/Generater/IdentificationGenerater.cs
@@ -9,21 +9,46 @@ namespace DwFramework.Core.Plugins
     /// <summary>
     /// 雪花算法
     /// </summary>
-    public class SnowflakeGenerater
+    public sealed class SnowflakeGenerater
     {
+        public sealed class SnowflakeIdInfo
+        {
+            public long ID { get; }
+            public DateTime StartTime { get; }
+            public long Timestamp { get; }
+            public DateTime Time => DateTime.UnixEpoch.AddSeconds(Timestamp);
+            public long WorkId { get; }
+            public long Sequence { get; }
+
+            /// <summary>
+            /// 构造函数
+            /// </summary>
+            /// <param name="id"></param>
+            /// <param name="startTime"></param>
+            public SnowflakeIdInfo(long id, DateTime? startTime = null)
+            {
+                ID = id;
+                StartTime = startTime != null ? startTime.Value : DateTime.UnixEpoch;
+                var timestamp = id >> (SnowflakeGenerater.WorkerIdBits + SnowflakeGenerater.SequenceBits);
+                Timestamp = timestamp + DateTime.UnixEpoch.GetTimeDiff(startTime);
+                WorkId = (id ^ (timestamp << (SnowflakeGenerater.WorkerIdBits + SnowflakeGenerater.SequenceBits))) >> SnowflakeGenerater.SequenceBits;
+                Sequence = id ^ (timestamp << (SnowflakeGenerater.WorkerIdBits + SnowflakeGenerater.SequenceBits) | WorkId << SnowflakeGenerater.SequenceBits);
+            }
+        }
+
+        public static readonly int TimestampBits = 41;
+        public static readonly int WorkerIdBits = 10;
+        public static readonly int SequenceBits = 12;
+
         private readonly object _lock = new object();
+        private long _currentTimestamp;
+        private long _currentSequence;
 
         public readonly long WorkerId;
         public readonly DateTime StartTime;
-        public readonly int TimestampBits = 41;
         public readonly long MaxTimestamp;
-        public readonly int WorkerIdBits = 10;
         public readonly long MaxWorkerId;
-        public readonly int SequenceBits = 12;
         public readonly long MaxSequence;
-
-        private long _currentTimestamp;
-        private long _currentSequence;
 
         /// <summary>
         /// 构造函数
@@ -35,10 +60,9 @@ namespace DwFramework.Core.Plugins
             MaxTimestamp = 2L << TimestampBits;
             MaxWorkerId = (2L << WorkerIdBits) - 1;
             MaxSequence = (2L << SequenceBits) - 1;
-
             if (workerId < 0 || workerId > MaxWorkerId) throw new Exception("机器ID超过上限");
             WorkerId = workerId;
-            StartTime = startTime != null ? startTime.Value : DateTime.Parse("1970.01.01");
+            StartTime = startTime != null ? startTime.Value : DateTime.UnixEpoch;
         }
 
         /// <summary>
@@ -51,8 +75,7 @@ namespace DwFramework.Core.Plugins
             {
                 _currentSequence++;
                 if (_currentSequence > MaxSequence) Thread.Sleep(1);
-
-                var timestamp = StartTime.ToUniversalTime().GetTimeDiff(DateTime.UtcNow);
+                var timestamp = StartTime.GetTimeDiff(DateTime.Now);
                 if (timestamp > MaxTimestamp) throw new Exception("时间戳容量不足,请调整StartTime");
                 if (timestamp < _currentTimestamp) throw new Exception("时间获取异常,请检查服务器时间");
                 else if (timestamp > _currentTimestamp)
@@ -60,9 +83,19 @@ namespace DwFramework.Core.Plugins
                     _currentTimestamp = timestamp;
                     _currentSequence = 0;
                 }
-
                 return _currentTimestamp << (WorkerIdBits + SequenceBits) | WorkerId << SequenceBits | _currentSequence;
             }
+        }
+
+        /// <summary>
+        /// 解析ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="startTime"></param>
+        /// <returns></returns>
+        public static SnowflakeIdInfo DecodeId(long id, DateTime? startTime = null)
+        {
+            return new SnowflakeIdInfo(id, startTime);
         }
     }
 
@@ -72,8 +105,6 @@ namespace DwFramework.Core.Plugins
     public static class UUIDGenerater
     {
         private static readonly object _lock = new object();
-
-        private static readonly DateTime _startTime = DateTime.Parse("1970.01.01");
         private static int _nonce = 0;
         private static long _currentTimestamp = 0;
 
@@ -99,7 +130,7 @@ namespace DwFramework.Core.Plugins
                 // Nonce
                 _nonce++;
                 if (_nonce > 2L << 4) Thread.Sleep(1);
-                var timestamp = _startTime.ToUniversalTime().GetTimeDiff(DateTime.UtcNow);
+                var timestamp = DateTime.UnixEpoch.GetTimeDiff(DateTime.UtcNow);
                 if (timestamp < _currentTimestamp) throw new Exception("时间获取异常,请检查服务器时间");
                 else if (timestamp > _currentTimestamp)
                 {
