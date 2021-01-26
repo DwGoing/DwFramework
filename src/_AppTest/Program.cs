18ff2034a54ef3869cfb22cab942f30b5248fabf
DwGoing <jianghy1209@163.com>
2021-01-26 11:25:34 +0800

    更新示例

--- src/_AppTest/Program.cs
+++ src/_AppTest/Program.cs
@@ -3,43 +3,28 @@ using System.Threading.Tasks;
 using DwFramework.Core;
 using DwFramework.Core.Plugins;
 using DwFramework.Core.Extensions;
-using DwFramework.WebSocket;
-
-using System.Collections.Generic;
-using System.Threading;
 
 namespace _AppTest
 {
+    class A
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public B B { get; set; }
+    }
+
+    class B
+    {
+        public int Age { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             try
             {
-                //var host = new ServiceHost();
-                //host.AddJsonConfig("WebSocket.json");
-                //host.RegisterLog();
-                //host.RegisterWebSocketService();
-                //host.OnInitialized += p =>
-                //{
-                //    var w = p.GetWebSocketService();
-                //    w.OnConnect += (c, a) => Console.WriteLine(c.ID);
-                //    w.OnReceive += (c, a) =>
-                //    {
-                //        var d = System.Text.Encoding.UTF8.GetString(a.Data);
-                //        Console.WriteLine(d);
-                //        if (d == "exit") c.CloseAsync(System.Net.WebSockets.WebSocketCloseStatus.PolicyViolation).Wait();
-                //    };
 
-                //    var client = new WebSocketClient();
-                //    client.OnClose += a =>
-                //    {
-                //        Console.WriteLine(a.CloseStatus);
-                //    };
-                //    client.ConnectAsync("ws://127.0.0.1:10090").Wait();
-                //    client.SendAsync(System.Text.Encoding.UTF8.GetBytes("exit")).Wait();
-                //};
-                //host.Run();
             }
             catch (Exception ex)
             {
