11cfc4bf09d6370102a6abc6fd516ffe36ced22b
Smilecl <chenjiahao@xcewell.cn>
2021-01-27 16:54:53 +0800

    ADD：添加WebApi示例

--- example/DwFramework.Example.WebAPI/Program.cs
+++ example/DwFramework.Example.WebAPI/Program.cs
@@ -12,6 +12,10 @@ namespace DwFramework.Example.WebAPI
             {
                 var host = new ServiceHost(configFilePath: "Config.json");
                 host.RegisterWebAPIService<Startup>();
+                host.OnInitialized += p =>
+                {
+                    var a = p.GetService<WebAPIService>();
+                };
                 host.Run();
             }
             catch (Exception ex)
