c98743183f11787fc5b34fd967be729b28011191
DwGoing <jianghy1209@163.com>
2021-02-23 13:26:13 +0800

    ADD：新增XML反序列化

--- src/Module/DwFramework.Core/Extensions/ObjectExtension.cs
+++ src/Module/DwFramework.Core/Extensions/ObjectExtension.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Text.Json;
+using System.Xml;
+using System.Xml.Serialization;
 using Mapster;
 
 namespace DwFramework.Core.Extensions
@@ -60,7 +64,7 @@ namespace DwFramework.Core.Extensions
         /// <param name="type"></param>
         /// <param name="options"></param>
         /// <returns></returns>
-        public static object ToObject(this string json, Type type, JsonSerializerOptions options = null)
+        public static object FromJson(this string json, Type type, JsonSerializerOptions options = null)
         {
             return JsonSerializer.Deserialize(json, type, options);
         }
@@ -70,10 +74,67 @@ namespace DwFramework.Core.Extensions
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="json"></param>
+        /// <param name="options"></param>
         /// <returns></returns>
-        public static T ToObject<T>(this string json, JsonSerializerOptions options = null)
+        public static T FromJson<T>(this string json, JsonSerializerOptions options = null)
         {
             return JsonSerializer.Deserialize<T>(json, options);
+        }
+
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="type"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static string ToXml(this object obj, Type type, Encoding encoding = null)
+        {
+            encoding ??= Encoding.UTF8;
+            using var output = new MemoryStream();
+            using var writer = new XmlTextWriter(output, encoding);
+            var serializer = new XmlSerializer(type);
+            serializer.Serialize(writer, obj);
+            return encoding.GetString(output.ToArray());
+        }
+
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static string ToXml<T>(this T obj, Encoding encoding = null)
+        {
+            return ToXml(obj, typeof(T), encoding);
+        }
+
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <param name="xml"></param>
+        /// <param name="type"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static object FromXml(this string xml, Type type, Encoding encoding = null)
+        {
+            encoding ??= Encoding.UTF8;
+            using var input = new MemoryStream(encoding.GetBytes(xml));
+            var serializer = new XmlSerializer(type);
+            return serializer.Deserialize(input);
+        }
+
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="xml"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static T FromXml<T>(this string xml, Encoding encoding = null)
+        {
+            return xml.FromXml(typeof(T), encoding).ConvertTo<T>();
         }
 
         /// <summary>
@@ -100,7 +161,7 @@ namespace DwFramework.Core.Extensions
         {
             encoding ??= Encoding.UTF8;
             var json = encoding.GetString(bytes);
-            return json.ToObject(type);
+            return json.FromJson(type);
         }
 
         /// <summary>
@@ -114,7 +175,7 @@ namespace DwFramework.Core.Extensions
         {
             encoding ??= Encoding.UTF8;
             var json = encoding.GetString(bytes);
-            return json.ToObject<T>();
+            return json.FromJson<T>();
         }
     }
 }
