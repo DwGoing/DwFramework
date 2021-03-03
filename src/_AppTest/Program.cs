27f59eb282962fc2161dd8229c94d85171c2d4d0
DwGoing <jianghy1209@163.com>
2021-03-03 18:34:37 +0800

    测试代码

--- src/_AppTest/Program.cs
+++ src/_AppTest/Program.cs
@@ -13,13 +13,21 @@ using Autofac;
 
 namespace _AppTest
 {
-    [Registerable(lifetime:Lifetime.Singleton,isAutoActivate:true)]
-    public class A
+    public sealed class WebSocketMessage
     {
-        public A(WebSocketService s)
-        {
+        public string Method { get; set; }
+        public object[] Params { get; set; }
+    }
 
-        }
+    public sealed class LoginInfo
+    {
+        public string Type { get; set; }
+        public string Token { get; set; }
+        public string Username { get; set; }
+        public string Passowrd { get; set; }
+        public string Uid { get; set; }
+        public string OriginCode { get; set; }
+        public string AccountType { get; set; }
     }
 
     class Program
@@ -28,20 +36,36 @@ namespace _AppTest
         {
             try
             {
-                // var c = new WebSocketClient();
-                // await c.ConnectAsync("ws://localhost:6002");
-                // await c.SendAsync(Encoding.UTF8.GetBytes("Hello"));
-                var host = new ServiceHost();
-                host.AddJsonConfig("Config.json");
-                host.RegisterLog();
-                host.RegisterTcpService("Tcp");
-                host.RegisterWebSocketService("WebSocket");
-                host.RegisterFromAssemblies();
-                host.OnInitialized += p =>
+                var c = new WebSocketClient();
+                c.OnReceive += args =>
                 {
-
+                    var res = args.Data.FromJsonBytes<ResultInfo>();
                 };
-                await host.RunAsync();
+                await c.ConnectAsync("ws://localhost:6002");
+                await c.SendAsync(new WebSocketMessage()
+                {
+                    Method = "Auth.Login",
+                    Params = new object[] { new LoginInfo(){
+                        Type="t",
+                        Username = "commkit",
+                        Passowrd = "123456",
+                        Uid="330624197207315918",
+                        OriginCode="3306240011",
+                        AccountType="2"
+                    }}
+                }.ToJsonBytes());
+                Console.Read();
+                // var host = new ServiceHost();
+                // host.AddJsonConfig("Config.json");
+                // host.RegisterLog();
+                // host.RegisterTcpService("Tcp");
+                // host.RegisterWebSocketService("WebSocket");
+                // host.RegisterFromAssemblies();
+                // host.OnInitialized += p =>
+                // {
+
+                // };
+                // await host.RunAsync();
             }
             catch (Exception ex)
             {
