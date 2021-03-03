e5176674eb607619a5ca4255eb7ba2fb8464a375
DwGoing <jianghy1209@163.com>
2021-03-03 14:32:33 +0800

    测试代码

--- src/_AppTest/Program.cs
+++ src/_AppTest/Program.cs
@@ -7,14 +7,18 @@ using System.Threading.Tasks;
 using DwFramework.Core;
 using DwFramework.Core.Plugins;
 using DwFramework.Core.Extensions;
+using DwFramework.WebSocket;
 using DwFramework.Socket;
+using Autofac;
 
 namespace _AppTest
 {
     public class A
     {
-        public int a { get; set; }
-        public string b { get; set; }
+        public A(WebSocketService s)
+        {
+
+        }
     }
 
     class Program
@@ -23,22 +27,18 @@ namespace _AppTest
         {
             try
             {
+                // var c = new WebSocketClient();
+                // await c.ConnectAsync("ws://localhost:6002");
+                // await c.SendAsync(Encoding.UTF8.GetBytes("Hello"));
                 var host = new ServiceHost();
                 host.AddJsonConfig("Config.json");
                 host.RegisterLog();
                 host.RegisterTcpService("Tcp");
+                host.RegisterWebSocketService("WebSocket");
+                host.RegisterType<A>().SingleInstance().AutoActivate();
                 host.OnInitialized += p =>
                 {
-                    var s = p.GetTcpService();
-                    s.OnConnect += (c, e) => Console.WriteLine(1);
-                    s.OnReceive += (c, e) =>
-                    {
-                        Console.WriteLine(Encoding.UTF8.GetString(e.Data));
-                        var data = new { A = "a", B = 123 }.ToJson();
-                        var msg = $"HTTP/1.1 200 OK\r\nContent-Type:application/json;charset=UTF-8\r\nContent-Length:{data.Length}\r\nConnection:close\r\n\r\n{data}";
-                        _ = c.SendAsync(Encoding.UTF8.GetBytes(msg));
-                    };
-                    s.OnClose += (c, e) => Console.WriteLine($"{c.ID}已断开");
+
                 };
                 await host.RunAsync();
             }
