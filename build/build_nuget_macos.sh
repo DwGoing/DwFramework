8b73c8a54956d4f8065a53451d906890ecd6c8e3
DwGoing <jianghy1209@163.com>
2021-03-03 16:24:48 +0800

    调整版本号生成与5.0.x一致

--- build/build_nuget_macos.sh
+++ build/build_nuget_macos.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
-usage="Usage:\n-c|--configuration <CONFIGURATION>\n-o|--output <OUTPUT_DIRECTORY>\n-s|--suffix <VERSION_SUFFIX>\n-v|--version <VERSION>"
+usage="Usage:\n-c|--configuration <CONFIGURATION>\n-o|--output <OUTPUT_DIRECTORY>\n-mv|--main-version <MAIN_VERSION>\n-sv|--sub-version <SUB_VERSION>\n-t|--start-year <START_YEAR>\n-s|--suffix <SUFFIX>"
 # 参数校验
-if [ $# -eq 0 ]; then
-    echo -e usage
+if [[ $# -eq 0 ]]; then
+    echo -e $usage
     exit 1
 fi
-args=$(getopt cosv $*)
+args=$(getopt -o "c:o:mv:sv:t:s:" $*)
 if [ $? -ne 0 ]; then
-    echo -e usage
+    echo -e $usage
     exit 2
 fi
+
 tag=""
 configuration=Debug
 output=.
-version=""
-reversion=""
-suffix=""
-startYear=2018
+mainVersion=""
+subVersion=""
+startYear=$(date +%Y)
+suffix=rc
+
 file=${@: -1}
 for i; do
     case $i in
-    -c | --configuration | -o | --output | -s | --suffix | -v | --buildversion)
+    -c | --configuration | -o | --output | -mv | --main-version | -sv | --sub-version | -t | --start-year | -s | --suffix)
         tag=$i
         ;;
     *)
@@ -33,12 +35,20 @@ for i; do
             output=$i
             tag=""
             ;;
-        -s | --suffix)
-            suffix=$i
+        -mv | --mian-version)
+            mainVersion=$i
             tag=""
             ;;
-        -v | --version)
-            version=$i
+        -sv | --sub-version)
+            subVersion=$i
+            tag=""
+            ;;
+        -t | --start-year)
+            startYear=$i
+            tag=""
+            ;;
+        -s | --suffix)
+            suffix=$i
             tag=""
             ;;
         esac
@@ -46,40 +56,40 @@ for i; do
     esac
 done
 
-if [ ! -f "$file" ]; then
+if [[ ! -f "$file" ]]; then
     echo "文件不存在!"
     exit 3
 fi
 
-if [ "${file##*.}"x != "csproj"x ]; then
+if [[ "${file##*.}"x != "csproj"x ]]; then
     echo "文件格式不支持!"
     exit 4
 fi
 
-if [ "$version"x == ""x ]; then
-    echo "未提供Version!"
+if [[ "$mainVersion"x == ""x || "$subVersion"x == ""x ]]; then
+    echo "未提供主版本号或子版本号!"
     exit 5
 fi
 
 currentYear=$(date +%Y)
-if [[ $version -lt 0 || $(expr $currentYear - $startYear) -lt $version ]]; then
-    echo "Version不可用!"
+offset=$(expr $currentYear - $startYear)
+if [[ $offset -lt 0 ]]; then
+    echo "起始年不可用!"
+    exit 5
+fi
+
+if [[ "$suffix"x == ""x ]]; then
+    echo "后缀不可用!"
     exit 5
 fi
 
 # 计算时间戳版本号
-start=$(date -j -f %Y-%m-%dT%H:%M:%S $(expr $startYear + $version)-01-01T00:00:00 +%s)
+start=$(date -j -f %Y-%m-%dT%H:%M:%S $startYear-01-01T00:00:00 +%s)
 current=$(date +%s)
 timestamp=$(expr $current - $start)
 week=$(expr $timestamp / 604800)
-buildVersion=$version.$week.$(expr $(expr $timestamp % 604800) / 60)
+buildVersion=$mainVersion.$subVersion.$week-$suffix.$(expr $(expr $timestamp % 604800) / 60)
 sed -i "" "s/\(<BuildVersion>\)[^<]*\(<\)/\1$buildVersion\2/g" $file
-
-# 版本后缀
-if [ "$suffix"x != ""x ]; then
-    suffix="-"$suffix
-fi
-sed -i "" "s/\(<VersionSuffix>\)[^<]*\(<\)/\1$suffix\2/g" $file
 
 # 构建
 dotnet pack -c $configuration -o $output $file
