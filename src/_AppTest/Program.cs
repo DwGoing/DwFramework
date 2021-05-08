7c56d3b84988097638cf1d28d040c7412bff9d56
DwGoing <jianghy1209@163.com>
2021-05-08 13:52:25 +0800

    📃 docs: 更新文档

--- src/_AppTest/Program.cs
+++ src/_AppTest/Program.cs
@@ -6,34 +6,55 @@ using System.Threading.Tasks;
 using DwFramework.Core;
 using DwFramework.Core.Plugins;
 using DwFramework.Core.Extensions;
-using SqlSugar;
 
-namespace _AppTest
+// 定义接口
+public interface ITestInterface
 {
-    class Program
+    void TestMethod(string str);
+}
+
+// 定义实现
+[Registerable]
+public class TestClass : ITestInterface
+{
+    // 要拦截的函数必须是virtual或override
+    public virtual void TestMethod(string str)
     {
-        static async Task Main(string[] args)
+        Console.WriteLine($"TestClass:{str}");
+    }
+}
+
+/// <summary>
+/// 构造拦截器
+/// 1.继承BaseInterceptor
+/// 2.重写OnCalling(CallInfo info)函数
+/// 3.重写OnCalled(CallInfo info)函数
+/// </summary>
+public class MyInterceptor : BaseInterceptor
+{
+    public override void OnCalling(CallInfo info)
+    {
+        Console.WriteLine("OnCalling");
+    }
+
+    public override void OnCalled(CallInfo info)
+    {
+        Console.WriteLine("OnCalled");
+    }
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var host = new ServiceHost();
+        host.RegisterInterceptors(typeof(MyInterceptor));
+        host.RegisterType<TestClass>().AddClassInterceptors(typeof(MyInterceptor));
+        host.OnInitialized += p =>
         {
-            try
-            {
-                var host = new ServiceHost();
-                host.AddJsonConfig("Config.json");
-                host.RegisterLog();
-                host.RegisterFromAssemblies();
-                host.OnInitializing += p =>
-                {
-                };
-                host.OnInitialized += p =>
-                {
-                };
-                await host.RunAsync();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                Console.Read();
-            }
-        }
+            p.GetService<TestClass>().TestMethod("Hi");
+        };
+        await host.RunAsync();
     }
 }
 
