1089f60cd23a5adde7a3e574236f5bfb3ef4d2ea
DwGoing <jianghy1209@163.com>
2021-06-16 16:46:10 +0800

    🧪 test: 增加Core示例

--- example/CoreExample/Program.cs
+++ example/CoreExample/Program.cs
@@ -3,9 +3,10 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Autofac;
+using Autofac.Extras.DynamicProxy;
+using NLog;
 using DwFramework.Core;
 using DwFramework.Core.AOP;
-using ilib;
 
 namespace CoreExample
 {
@@ -13,34 +14,27 @@ namespace CoreExample
     {
         static async Task Main(string[] args)
         {
-            var x = ModuleManager.LoadModule<IClass1>(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "lib/lib.dll"));
-            ModuleManager.UnloadModule<IClass1>();
-            // var y = ModuleManager.LoadModule<IClass1>(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "lib/lib.dll"));
-            await Task.Delay(1000);
-            Console.WriteLine(x.Do(5, 8));
-            // Console.WriteLine(y.Do(5, 2));
-
-            // var host = new ServiceHost();
-            // host.ConfigureLogging(builder => builder.UserNLog());
-            // host.ConfigureContainer(builder =>
-            // {
-            //     builder.Register(c => new LoggerInterceptor(invocation => (
-            //         $"{invocation.TargetType.Name}InvokeLog",
-            //         LogLevel.Debug,
-            //         "\n========================================\n"
-            //         + $"Method:\t{invocation.Method}\n"
-            //         + $"Args:\t{string.Join('|', invocation.Arguments)}\n"
-            //         + $"Return:\t{invocation.ReturnValue}\n"
-            //         + "========================================"
-            //     )));
-            //     builder.RegisterType<A>().As<I>().EnableInterfaceInterceptors();
-            //     builder.RegisterType<B>().As<I>().EnableInterfaceInterceptors();
-            // });
-            // host.OnHostStarted += provider =>
-            // {
-            //     foreach (var item in provider.GetServices<I>()) item.Do(5, 6);
-            // };
-            // await host.RunAsync();
+            var host = new ServiceHost();
+            host.ConfigureLogging(builder => builder.UserNLog());
+            host.ConfigureContainer(builder =>
+            {
+                builder.Register(c => new LoggerInterceptor(invocation => (
+                    $"{invocation.TargetType.Name}InvokeLog",
+                    LogLevel.Debug,
+                    "\n========================================\n"
+                    + $"Method:\t{invocation.Method}\n"
+                    + $"Args:\t{string.Join('|', invocation.Arguments)}\n"
+                    + $"Return:\t{invocation.ReturnValue}\n"
+                    + "========================================"
+                )));
+                builder.RegisterType<A>().As<I>().EnableInterfaceInterceptors();
+                builder.RegisterType<B>().As<I>().EnableInterfaceInterceptors();
+            });
+            host.OnHostStarted += provider =>
+            {
+                foreach (var item in provider.GetServices<I>()) item.Do(5, 6);
+            };
+            await host.RunAsync();
         }
     }
 
