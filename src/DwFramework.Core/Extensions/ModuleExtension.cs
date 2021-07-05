b7b0d88a4a3066bc31f84ce66084e98013bb551b
DwGoing <jianghy1209@163.com>
2021-07-05 14:02:32 +0800

    ✨ feat: 新增动态模块导入

--- src/DwFramework.Core/Extensions/ModuleExtension.cs
+++ src/DwFramework.Core/Extensions/ModuleExtension.cs
@@ -1,7 +1,18 @@
-namespace DwFramework.Core.Extensions
+using System;
+using Autofac;
+
+namespace DwFramework.Core
 {
     public static class ModuleExtension
     {
-
+        /// <summary>
+        /// 导入模块
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="modulePath"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static ServiceHost ImportModule<T>(this ServiceHost host, string modulePath)
+            => host.ConfigureContainer(builder => builder.Register(_ => ModuleManager.LoadModule<T>(modulePath)));
     }
 }
