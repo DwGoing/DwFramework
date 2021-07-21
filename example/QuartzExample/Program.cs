f886f65497f65a6d82dc9d1cfe76e44762753d79
DwGoing <jianghy1209@163.com>
2021-07-21 22:29:41 +0800

    ✨ feat(): 新增Quartz示例
    Signed-off-by: DwGoing <jianghy1209@163.com>

---
+++ example/QuartzExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Quartz;
+using Autofac;
+using DwFramework.Core;
+using DwFramework.Quartz;
+
+namespace QuartzExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = new ServiceHost();
+            host.ConfigureContainer(b =>
+            {
+                b.RegisterType<A>().SingleInstance();
+                b.RegisterType<Job>();
+            });
+            host.ConfigureQuartz();
+            host.OnHostStarted += async p =>
+            {
+                var s = p.GetTaskSchedule();
+                var ss = await s.CreateSchedulerAsync("X", true);
+                await s.CreateJobAsync<Job>("X", "1/5 * * * * ?");
+                await ss.Start();
+            };
+            await host.RunAsync();
+        }
+    }
+
+    public class A
+    {
+        public readonly Guid Id = Guid.NewGuid();
+    }
+
+    public class Job : IJob
+    {
+        private A _a;
+
+        public Job(A a)
+        {
+            _a = a;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine(_a.Id);
+            return Task.CompletedTask;
+        }
+    }
+}
