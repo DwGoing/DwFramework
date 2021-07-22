ae6f7ed5175940177cacc1005e19f0a2f741ba73
DwGoing <jianghy1209@163.com>
2021-07-22 10:15:28 +0800

    新增雪花ID生成器注入

--- src/DwFramework.Core/Plugins/Generator/GeneratorExtension.cs
+++ src/DwFramework.Core/Plugins/Generator/GeneratorExtension.cs
@@ -6,13 +6,20 @@ namespace DwFramework.Core.Generator
     public static class GeneratorExtension
     {
         /// <summary>
-        /// 配置全局雪花生成器
+        /// 配置雪花生成器
         /// </summary>
         /// <param name="host"></param>
         /// <param name="workerId"></param>
         /// <param name="startTime"></param>
+        /// <param name="isGlobal"></param>
         /// <returns></returns>
-        public static ServiceHost cSnowflakeGenerator(this ServiceHost host, long workerId, DateTime startTime)
-            => host.ConfigureContainer(builder => builder.Register(_ => new SnowflakeGenerator(workerId, startTime)).SingleInstance());
+        public static ServiceHost cSnowflakeGenerator(this ServiceHost host, long workerId, DateTime startTime, bool isGlobal = true)
+        {
+            return host.ConfigureContainer(builder =>
+            {
+                var registration = builder.Register(_ => new SnowflakeGenerator(workerId, startTime));
+                if (isGlobal) registration.SingleInstance();
+            });
+        }
     }
 }
