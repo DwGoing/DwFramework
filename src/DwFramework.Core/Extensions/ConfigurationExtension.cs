b3491d2e4a9d4c81026606c6016e86f77f5e3fdf
DwGoing <jianghy1209@163.com>
2021-07-23 10:07:50 +0800

    🦄 refactor(): 优化IConfiguration扩展

--- src/DwFramework.Core/Extensions/ConfigurationExtension.cs
+++ src/DwFramework.Core/Extensions/ConfigurationExtension.cs
@@ -9,25 +9,29 @@ namespace DwFramework.Core
         /// <summary>
         /// 获取配置
         /// </summary>
-        /// <param name="provider"></param>
+        /// <param name="configuration"></param>
         /// <param name="path"></param>
-        /// <typeparam name="T"></typeparam>
         /// <returns></returns>
-        public static T ParseConfiguration<T>(this IServiceProvider provider, string path = null)
-        {
-            var root = provider.GetService<IConfiguration>();
-            if (root == null) return default;
-            return string.IsNullOrEmpty(path) ? root.Get<T>() : root.GetSection(path).Get<T>();
-        }
+        public static IConfiguration GetConfiguration(this IConfiguration configuration, string path = null)
+            => configuration == null || string.IsNullOrEmpty(path) ? configuration : configuration.GetSection(path);
 
         /// <summary>
-        /// 获取配置
+        /// 解析配置
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="configuration"></param>
         /// <param name="path"></param>
         /// <returns></returns>
-        public static T ParseConfiguration<T>(this IConfiguration configuration, string path = null)
-            => string.IsNullOrEmpty(path) ? configuration.Get<T>() : configuration.GetSection(path).Get<T>();
+        public static T ParseConfiguration<T>(this IConfiguration configuration, string path = null) => configuration.Get<T>();
+
+        /// <summary>
+        /// 解析配置
+        /// </summary>
+        /// <param name="provider"></param>
+        /// <param name="path"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T ParseConfiguration<T>(this IServiceProvider provider, string path = null)
+            => provider.GetService<IConfiguration>().ParseConfiguration<T>();
     }
 }
