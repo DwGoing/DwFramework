df90b0540cc68ec982e01245d129eba7a29227a8
DwGoing <jianghy1209@163.com>
2021-09-01 11:55:09 +0800

    🦄 refactor: 移除部分未验证扩展

--- src/DwFramework.Core/Extensions/IEnumerableExtension.cs
+++ src/DwFramework.Core/Extensions/IEnumerableExtension.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace DwFramework.Core
 {
@@ -20,86 +18,6 @@ namespace DwFramework.Core
         {
             var hash = new HashSet<TKey>();
             return source.Where(item => hash.Add(selector(item)));
-        }
-
-        /// <summary>
-        /// 添加多个元素
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="collection"></param>
-        /// <param name="values"></param>
-        public static void AddRange<T>(this ICollection<T> collection, params T[] values)
-        {
-            foreach (var item in values) collection.Add(item);
-        }
-
-        /// <summary>
-        /// 添加符合条件的多个元素
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="collection"></param>
-        /// <param name="predicate"></param>
-        /// <param name="values"></param>
-        public static void AddRangeIf<T>(this ICollection<T> collection, Func<T, bool> predicate, params T[] values)
-        {
-            foreach (var item in values) if (predicate(item)) collection.Add(item);
-        }
-
-        /// <summary>
-        /// 添加不重复的元素
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="collection"></param>
-        /// <param name="values"></param>
-        public static void AddRangeIfNotContains<T>(this ICollection<T> collection, params T[] values)
-        {
-            foreach (var item in values) if (!collection.Contains(item)) collection.Add(item);
-        }
-
-        /// <summary>
-        /// 移除符合条件的元素
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="collection"></param>
-        /// <param name="predicate"></param>
-        public static void RemoveWhere<T>(this ICollection<T> collection, Func<T, bool> predicate)
-        {
-            var removable = collection.Where(predicate);
-            foreach (var item in removable) collection.Remove(item);
-        }
-
-        /// <summary>
-        /// 在元素之后添加元素
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="list"></param>
-        /// <param name="predicate">条件</param>
-        /// <param name="value">值</param>
-        public static void InsertAfter<T>(this IList<T> list, Func<T, bool> predicate, T value)
-        {
-            var tmp = list.Select((item, index) => new { item, index }).Where(p => predicate(p.item)).OrderByDescending(p => p.index);
-            foreach (var item in tmp)
-            {
-                if (item.index + 1 == list.Count) list.Add(value);
-                else list.Insert(item.index + 1, value);
-            };
-        }
-
-        /// <summary>
-        /// 在元素之后添加元素
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="list"></param>
-        /// <param name="index">索引位置</param>
-        /// <param name="value">值</param>
-        public static void InsertAfter<T>(this IList<T> list, int index, T value)
-        {
-            var tmp = list.Select((v, i) => new { Value = v, Index = i }).Where(p => p.Index == index).OrderByDescending(p => p.Index);
-            foreach (var item in tmp)
-            {
-                if (item.Index + 1 == list.Count) list.Add(value);
-                else list.Insert(item.Index + 1, value);
-            };
         }
 
         /// <summary>
